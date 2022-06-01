3051d11fd2ab450ea0b8542c9b6698cb8058573a
Paulo Costa <paulotcj@gmail.com>
2022-06-01 15:54:44 -0400

    .

---
+++ arrays/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 3, 7, 9, 2 };
+
+int findThis = 11;
+
+var answer = TwoSum.BruteForce(arr, findThis);
+TwoSum.PrintResult(arrValues: arr, answer: answer, whatToFind: findThis);
+
+public static class TwoSum
+{
+    public static int[] BruteForce(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int numberToFind = target - arr[i];
+
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (numberToFind == arr[j]) 
+                { 
+                    return new int[] { i, j }; 
+                }
+            }
+        }
+
+        return null;
+    }
+
+    //------------------------
+
+    public static void PrintResult(int[] arrValues, int[] answer, int whatToFind)
+    {
+        if (answer == null || answer.Length != 2) 
+        { 
+            Console.WriteLine($"Input is null or of invalid length - No answer was found");
+            return;
+        }
+        Console.WriteLine($"Seeking for {whatToFind}");
+        Console.WriteLine($"    Answer - Arr[{answer[0]}] = {arrValues[answer[0]]} , Arr[{answer[1]}] = {arrValues[answer[1]]} ");
+    }
+}
+
+
