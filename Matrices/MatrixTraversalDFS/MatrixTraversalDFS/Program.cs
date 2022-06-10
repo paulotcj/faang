fcc78d52dee98c5440ff64b78de008c257f12b5d
Paulo Costa <paulotcj@gmail.com>
2022-06-10 03:58:08 -0400

    .

---
+++ Matrices/MatrixTraversalDFS/MatrixTraversalDFS/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[][] directions = new int[][] {
+    new int[]{-1,  0 }, //up
+    new int[]{ 0 , 1 }, //right
+    new int[]{ 1 , 0 }, //down
+    new int[]{ 0 ,-1 }  //left
+};
+
+int[][] testMatrix = new int[][] { 
+    new int[]{ 1 ,  2 ,  3 ,  4 ,  5 },
+    new int[]{ 6 ,  7 ,  8 ,  9 , 10 },
+    new int[]{11 , 12 , 13 , 14 , 15 },
+    new int[]{16 , 17 , 18 , 19 , 20 },
+};
