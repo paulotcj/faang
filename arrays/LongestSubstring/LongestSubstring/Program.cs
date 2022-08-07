eddc38d66e75f86e7bd8f875b91358246d31c734
Paulo Costa <paulotcj@gmail.com>
2022-08-06 20:58:40 -0400

    .

---
+++ arrays/LongestSubstring/LongestSubstring/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//const string str = "abccabb";
+
+const string str = "abcbdaac";
+
+Solution solution = new();
+
+int result = solution.LengthOfLongestSubstring(str);
+
+Console.WriteLine($"Longest substring: {result}");
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string str)
+    {
+        if (str.Length <= 1) { return str.Length; }
+
+        Dictionary<char, int> seen = new();
+        int left = 0, longest = 0;
+
+
+        //----------
+        for (int right = 0; right < str.Length; right ++) //we are moving the right pointer continuously to the right
+        {
+            char curr = str[right];
+
+            //main logic: check whether we have this character in the dictionary, and also important to remember
+            //  we dont clear the dictionary when we move the window, so dictionary entries stay in there regardless
+            //
+            // check if we have 'curr' character in the dictionary
+            // If we DON'T have it: 
+            //       - add this 'curr' character to the dictionary using the 'right' pointer as a indicator of its location
+            // ELSE (meaning if we do have it):
+            //       - check if this index is within our 'window' frame
+            //         if it is we need to move the left pointer to the position where this characters was last seen plus 1
+            //         and that's because the 'right' pointer is sitting right at where this 'repeated' character is
+            //       - update the position of the last occurent of the current character with the right pointer, as it's sitting
+            //          right there
+            //
+            // THEN get the max: either the current longest substring sequence, or the current substring within our window
+            if (seen.ContainsKey(curr) == false)
+            { seen.Add(curr, right); }
+            else
+            {
+                int idxPrevSeenCurrChar = seen[curr];
+
+                if (left <= idxPrevSeenCurrChar) //is this repeadted char within the window frame? because if it's not it doesn't matter
+                {
+                    left = idxPrevSeenCurrChar + 1; // move the left pointer to the position where this characters was last seen plus 1
+                                                    //because the 'right' pointer is sitting right at where this 'repeated' character is
+                }
+
+                seen[curr] = right; //update its last position
+            }
+
+            longest = Math.Max(longest, ( right - left + 1 ) ); //which one is bigger? longest or the current substring being investigated?
+
+            
+        }
+        //----------
+        return longest;
+    }
+
+
+    public int LengthOfLongestSubstring_Suboptimal(string str)
+    {
+        if (str.Length <= 1) { return str.Length; }
+
+        int longest = 0;
+
+        for (int left = 0; left < str.Length; left++) //for every loop we move the left pointer one step to the right
+        {
+            Dictionary<char, int> seenChars = new();
+            int currentLenght = 0;
+
+            for (int right = left; right < str.Length; right++) 
+            {
+                char currentChar = str[right];
+
+                if (seenChars.ContainsKey(currentChar) == false)
+                {
+                    currentLenght++;
+                    seenChars.Add(currentChar, right);
+                    longest = Math.Max(longest, currentLenght);
+                }
+                else 
+                {
+                    break; //in situations like this we are bailing out before the end of the array, identifying individual substrings
+                }
+            }
+
+        }
+
+        return longest;
+
+    }
+}
---
+++ arrays/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,182 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+string str = "A man, a plan, a canal: Panama";
+str = "aabcdxcbaa";
+Solution solution = new();
+bool result = false;
+
+
+str = "aabccbaa";
+result = solution.IsPalindrome_Reverse(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdcbaa";
+result = solution.IsPalindrome_Reverse(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdxcbaa";
+result = solution.IsPalindrome_Reverse(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: false");
+
+
+
+Console.WriteLine("-----------------------------------------------");
+
+
+str = "aabccbaa";
+result = solution.IsPalindrome_Outside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdcbaa";
+result = solution.IsPalindrome_Outside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdxcbaa";
+result = solution.IsPalindrome_Outside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: false");
+
+
+
+Console.WriteLine("-----------------------------------------------");
+str = "aabccbaa";
+result = solution.IsPalindrome_Inside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdcbaa";
+result = solution.IsPalindrome_Inside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: true");
+
+str = "aabcdxcbaa";
+result = solution.IsPalindrome_Inside(str);
+Console.WriteLine($"Is {str} Palindrome: {result} - expected: false");
+
+
+Console.WriteLine("-----------------------------------------------");
+
+
+str = "aba";
+result = solution.AlmostPalindrome(str);
+Console.WriteLine($"Is {str} almost a palindrome: {result} - expected: true");
+
+str = "abca";
+result = solution.AlmostPalindrome(str);
+Console.WriteLine($"Is {str} almost a palindrome: {result} - expected: true");
+
+str = "abc";
+result = solution.AlmostPalindrome(str);
+Console.WriteLine($"Is {str} almost a palindrome: {result} - expected: false");
+
+
+public class Solution
+{
+
+    //------------------------------
+    public bool AlmostPalindrome(string str)
+    {
+        str = System.Text.RegularExpressions.Regex.Replace(str, "[^A-Za-z0-9]", "");//.ToLower();
+
+        int left = 0;
+        int right = str.Length - 1;
+
+        while(left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return (
+                           SubPalindrome(str, left + 1, right) || 
+                           SubPalindrome(str, left, right - 1)
+                        );
+            }
+
+            left++;
+            right--;
+        }
+
+
+        return true;
+    }
+
+    public bool SubPalindrome(string str, int left, int right) //calculates the palindrome but without the overhead and set up
+    {
+        while ( left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+
+    //------------------------------
+
+
+
+    public bool IsPalindrome_Inside(string str)
+    {
+        str = System.Text.RegularExpressions.Regex.Replace(str, "[^A-Za-z0-9]", "").ToLower();
+
+        int left = str.Length / 2; //we don't need to round (floor) this value in C#
+        int right = left;
+
+        if ((str.Length % 2) == 0) { left--; }
+
+        while(left >= 0 && right < str.Length)
+        {
+            if (str[left] != str[right]) { return false; }
+
+            left--;
+            right++;
+        }
+
+
+
+
+        return true;
+
+    }
+
+
+    public bool IsPalindrome_Outside(string str)
+    {
+        str = System.Text.RegularExpressions.Regex.Replace(str, "[^A-Za-z0-9]", "").ToLower();
+
+        int left = 0;
+        for (int right = str.Length - 1; right >= 0; )
+        {
+            if (str[left] != str[right]) { return false; }
+
+            right--;
+            left++;
+        }
+
+        return true;
+    }
+
+
+    public bool IsPalindrome_Reverse(string str)
+    {
+
+        str = System.Text.RegularExpressions.Regex.Replace(str, "[^A-Za-z0-9]", "").ToLower();
+
+        //--------------------
+        // reverse string
+        char[] charStr = str.ToCharArray();
+        int j = 0;
+        for (int i = str.Length-1; i >= 0; i--)
+        {
+            charStr[j++] = str[i];
+        }
+        //--------------------
+
+        return String.Compare(str, new String(charStr)) == 0 ? true : false;
+
+
+    }
+}
