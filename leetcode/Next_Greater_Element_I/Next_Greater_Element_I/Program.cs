0a519d4fb7d1e8542ad775f516539f49e0071d3f
Paulo Costa <paulotcj@gmail.com>
2022-08-14 12:35:08 -0400

    .

---
+++ leetcode/Next_Greater_Element_I/Next_Greater_Element_I/Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//https://leetcode.com/problems/next-greater-element-i/
+
+int[] arr1;// = { 4, 1, 2 };
+int[] arr2;// = { 1, 3, 4, 2 };
+int[] response;
+
+Solution sol = new();
+
+Console.WriteLine("--------------------");
+arr1 = new int[] { 4, 1, 2 };
+arr2 = new int[] { 1, 3, 4, 2 };
+
+response = sol.NextGreaterElement(arr1, arr2);
+Console.WriteLine($"Response: [{sol.ArrayToString(response)}]  - Expected: [-1,3,-1]");
+Console.WriteLine("--------------------");
+
+
+
+arr1 = new int[] { 2, 4 };
+arr2 = new int[] { 1, 2, 3, 4 };
+
+response = sol.NextGreaterElement(arr1, arr2);
+Console.WriteLine($"Response: [{sol.ArrayToString(response)}]  - Expected: [3,-1]");
+Console.WriteLine("--------------------");
+
+
+
+public class Solution
+{
+    public int[] NextGreaterElement2(int[] a, int[] b)
+    {
+        if (a == null || b == null || a.Length == 0 || b.Length == 0 || a.Length > b.Length) { return new int[0]; }
+
+        int[] response = new int[a.Length];
+        int curr = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            curr = a[i];
+            int matchingIndex = FindMatchingIndex(b, curr);
+            if (matchingIndex == -1) { response[i] = -1; }
+            else
+            {
+
+            }
+
+
+        }
+    }
+
+    //------------------------------------------
+
+    public int ArraySeekGreaterElement(int[] arr, int idxWhereToStart, int valueToSeek)
+    { 
+
+
+        return -1;
+    }
+
+    //------------------------------------------
+
+public int FindMatchingIndex(int[] arr, int value)
+    {
+        int index;
+        for (int i = 0; i < arr.Length; i ++) 
+        {
+            if (arr[i] == value) { return i; }
+        }
+        return -1;
+    }
+
+
+
+
+    public int[] NextGreaterElement(int[] a, int[] b)
+    {
+        if (a == null || b == null || a.Length > b.Length)
+            return new int[0];
+
+        var s = new Stack<int>();
+        var o = new int[a.Length];
+        var d = new Dictionary<int, int>();
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            d[a[i]] = i;
+            o[i] = -1;
+        }
+
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (d.Count == 0)
+                break;
+
+            var cur = b[i];
+
+            while (s.Count > 0 && cur > s.Peek())
+            {
+                var prev = s.Pop();
+                if (d.ContainsKey(prev))
+                {
+                    o[d[prev]] = cur;
+                    d.Remove(prev);
+                }
+            }
+
+            s.Push(cur);
+        }
+
+        return o;
+    }
+    //-----------------------------------------------
+    public string ArrayToString(int[] arr)
+    {
+        string output = "";
+        for (int i = 0; i < arr.Length ; i++ )
+        {
+            output += arr[i] + ",";
+        }
+
+        return output;
+    }
+}
---
+++ leetcode/Number_of_Days_Between_Two_Dates/Number_of_Days_Between_Two_Dates/Program.cs
@@ -0,0 +1,134 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Solution sol = new();
+
+string date1;
+string date2;
+int result = 0;
+
+
+//Console.WriteLine("---------------------------------------");
+//date1 = "2019-06-29";
+//date2 = "2019-06-30";
+//result = sol.DaysBetweenDates2(date1, date2);
+//Console.WriteLine($"Days between dates {date2} and {date1} is : {result}");
+
+Console.WriteLine("---------------------------------------");
+date1 = "2020-01-15";
+date2 = "2019-12-31";
+result = sol.DaysBetweenDates2(date1, date2);
+Console.WriteLine($"Days between dates {date2} and {date1} is : {result}");
+
+
+
+
+
+
+public class Solution
+{
+
+    public int DaysBetweenDates2(string date1, string date2)
+    {
+        int d1 = CountSinceEpoch(date1);
+        int d2 = CountSinceEpoch(date2);
+        int days = d1 - d2;
+
+        //-------------
+
+        //int d3 = CountSinceEpoch2(date1);
+        //int d4 = CountSinceEpoch2(date2);
+        //int days_B = d3 - d4;
+
+
+
+        days = Math.Abs(days);
+        return days;
+    }
+
+
+
+
+    public int CountSinceEpoch(string date)
+    {
+        int[] monthDays = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        string[] dateSplit = date.Split("-");
+
+        int year    = int.Parse(dateSplit[0]);
+        int month   = int.Parse(dateSplit[1]);
+        int day     = int.Parse(dateSplit[2]);
+
+
+        //Add days in the year, considering leap years
+        //  note: we are only counting past years, not necessarily this year
+        for (int i = 1971; i < year; i++)
+        {
+            if (IsALeapYear(i)) { day += 366; }
+            else { day += 365; }
+        }
+
+        for (int i = 0; i < month; i++)
+        {
+            day += monthDays[i];
+        }
+
+
+        if (IsALeapYear(year) == true && month > 2) { day += 1; }
+
+
+        return day;
+
+    }
+
+    //public int CountSinceEpoch(String date)
+    //{
+
+    //    int[] monthDays = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    //    string[] dateSplit = date.Split("-");
+
+    //    int year = Int32.Parse(dateSplit[0]);
+    //    int month = Int32.Parse(dateSplit[1]);
+    //    int day = Int32.Parse(dateSplit[2]);
+
+    //    for (int i = 1971; i < year; i++)
+    //    {
+    //        day += IsALeapYear(i) ? 366 : 365;
+    //    }
+    //    for (int i = 1; i < month; i++)
+    //    {
+    //        if (IsALeapYear(year) && i == 2)
+    //        {
+    //            day += 1;
+    //        }
+    //        day += monthDays[i];
+    //    }
+    //    return day;
+    //}
+
+    public bool IsALeapYear(int year)
+    {
+        return ( (year % 4)     == 0 && //is year div by 4 but not by 100
+                 (year % 100)   != 0) || 
+                 (year % 400)    == 0;
+    }
+
+    //----------------------------------------------------
+
+    public int DaysBetweenDates3(string date1 , string  date2)
+    {
+        DateTime d1 = DateTime.ParseExact(date1, "yyyy-MM-dd", null);
+        DateTime d2 = DateTime.ParseExact(date2, "yyyy-MM-dd", null);
+
+        int days = (int)(d2 - d1).TotalDays;
+        days = Math.Abs(days);
+        return days;
+    }
+
+    public int DaysBetweenDates(string date1, string date2)
+    {
+        var d1 = DateTime.ParseExact(date1, "yyyy-MM-dd", null);
+        var d2 = DateTime.ParseExact(date2, "yyyy-MM-dd", null);
+        return Math.Abs((int)(d2 - d1).TotalDays);
+    }
+}
