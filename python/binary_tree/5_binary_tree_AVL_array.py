170adb8eecd8fe514627e827ad8727944d434246
Paulo Costa <paulotcj@gmail.com>
2023-10-04 22:08:28 -0400

    .

--- python/binary_tree/5_binary_tree_AVL_array.py
+++ python/binary_tree/5_binary_tree_AVL_array.py
@@ -1,3 +1,12 @@
+class Node:
+    def __init__(self, value) -> None:
+        # self.left = None
+        # self.right = None
+        self.value = value
+        # self.parent = None
+        self.height = 1
+
+
 class BinaryTreeArray:
     def __init__(self):
         self.tree = []
@@ -24,21 +33,20 @@ class BinaryTreeArray:
     def insert(self, value):
         # print(f'adding: {value}')
         if self.count + 1 > self.capacity: self.add_capacity()
-        
   
         idx = 0
         while True:
             if idx+1 > self.capacity:
-                # print('Error - Out of bound access attempt')
                 self.add_capacity()
-                # break
 
-            if self.tree[idx] == None:
-                self.tree[idx] = value
+
+            if self.tree[idx] == None: # spot found - add node here
+                new_obj = Node(value)
+                self.tree[idx] = new_obj
                 self.count +=1 
                 break
 
-            elif value < self.tree[idx]: #must go to the left side
+            elif value < self.tree[idx].value: #must go to the left side
                 idx = idx*2 + 1
 
             else: # by exclusion: must go to the right side
@@ -52,12 +60,20 @@ class BinaryTreeArray:
                 print('Item not found')
                 return None
             
-            elif value == self.tree[idx]: #exact match
+            elif value == self.tree[idx].value: #exact match
                 return self.tree[idx] 
-            elif value < self.tree[idx]: #must go to the left side
+            elif value < self.tree[idx].value: #must go to the left side
                 idx = idx*2 + 1
             else: # by exclusion: must go to the right side
                 idx = idx*2 + 2
+
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
+    def values_to_array(self):
+        #i.value if i is not None else None
+        return_obj = [ i.value if i is not None else None for i in self.tree]
+        return return_obj
+
 
         
                 
@@ -82,7 +98,8 @@ class Test_BinaryTreeArray:
 
         assert expected_capacity == tree.capacity
         assert expected_level == measured_level
-
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
     def test_capacity_7(self):
         #arrange
         tree = BinaryTreeArray()
@@ -97,7 +114,8 @@ class Test_BinaryTreeArray:
 
         assert expected_capacity == tree.capacity
         assert expected_level == tree.level
-
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
     def test_capacity_15(self):
         #arrange
         tree = BinaryTreeArray()
@@ -113,100 +131,77 @@ class Test_BinaryTreeArray:
 
         assert expected_capacity == tree.capacity
         assert expected_level == tree.level        
-
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
     def test_tree_insert_1_2_3_4_5(self):
         #arrange
         tree = BinaryTreeArray()
-
-        #act
         tree.insert(1) 
         tree.insert(2) 
         tree.insert(3) 
         tree.insert(4) 
         tree.insert(5) 
-
         expected_list = [1, None, 2, None, None, None, 3, None, None, None, None, None, None, None, 4, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 5]
 
-        for i in range(len(tree.tree)):
-            assert expected_list[i] == tree.tree[i]
 
-        assert 1 == tree.tree[0]
-        assert 2 == tree.tree[2]
-        assert 3 == tree.tree[6]
-        assert 4 == tree.tree[14]
-        assert 5 == tree.tree[30]
+        #act
+        result_list = tree.values_to_array()      
 
+
+        #assert
+        for i in range(len(result_list)):
+            assert expected_list[i] == result_list[i]
+
+        assert 1 == result_list[0]
+        assert 2 == result_list[2]
+        assert 3 == result_list[6]
+        assert 4 == result_list[14]
+        assert 5 == result_list[30]
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
     def test_lookup_1_2_3_4_5_6_7_8_9_10_11_all_must_be_found(self):
         #arrange
         tree = BinaryTreeArray()
-        tree.insert(1) 
-        tree.insert(2) 
-        tree.insert(3) 
-        tree.insert(4)
-        tree.insert(5)
-        tree.insert(6)
-        tree.insert(7)
-        tree.insert(8)
-        tree.insert(9)
-        tree.insert(10)
-        tree.insert(11)        
+        numbers = [1,2,3,4,5,6,7,8,9,10,11]
+        for i in numbers:
+            tree.insert(i)
 
+        lookup_result = []
+        for i in numbers:
+            temp = tree.lookup(i)
+            lookup_result.append( temp.value if temp is not None else None )  
 
         #act
-        lookup_result_1 = tree.lookup(1)
-        lookup_result_2 = tree.lookup(2)
-        lookup_result_3 = tree.lookup(3)
-        lookup_result_4 = tree.lookup(4)
-        lookup_result_5 = tree.lookup(5)
-        lookup_result_6 = tree.lookup(6)
-        lookup_result_7 = tree.lookup(7)
-        lookup_result_8 = tree.lookup(8)
-        lookup_result_9 = tree.lookup(9)
-        lookup_result_10 = tree.lookup(10)
-        lookup_result_11 = tree.lookup(11)
+        for i in range(len(numbers)):
+            assert numbers[i] == lookup_result[i]
 
-
-        assert lookup_result_1 == 1
-        assert lookup_result_2 == 2
-        assert lookup_result_3 == 3
-        assert lookup_result_4 == 4
-        assert lookup_result_5 == 5
-        assert lookup_result_6 == 6
-        assert lookup_result_7 == 7
-        assert lookup_result_8 == 8
-        assert lookup_result_9 == 9
-        assert lookup_result_10 == 10
-        assert lookup_result_11 == 11
-
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------
     def test_lookup_none_must_be_found(self):
         #arrange
         tree = BinaryTreeArray()
-        tree.insert(1) 
-        tree.insert(2) 
-        tree.insert(3) 
-        # tree.insert(4)
-        tree.insert(5)
-        tree.insert(6)
-        tree.insert(7)
-        tree.insert(8)
-        tree.insert(9)
-        tree.insert(10)
-        tree.insert(11)        
-
+        numbers_to_insert = [1,2,3,5,6,7,8,9,10,11]
+        numbers_not_to_be_found = [-1,0,12,4]
+        for i in numbers_to_insert:
+            tree.insert(i)
 
         #act
-        lookup_result_1 = tree.lookup(-1)
-        lookup_result_2 = tree.lookup(0)
-        lookup_result_3 = tree.lookup(12)
-        lookup_result_4 = tree.lookup(4)
+        lookup_results = []
+        for i in numbers_not_to_be_found:
+            temp = tree.lookup(i)
+            lookup_results.append( temp.value if temp is not None else None )  
 
 
+        #assert
+        for i in range(len(numbers_not_to_be_found)):
+            assert lookup_results[i] == None
 
-        assert lookup_result_1 == None
-        assert lookup_result_2 == None
-        assert lookup_result_3 == None
-        assert lookup_result_4 == None
-        
+
+     
+
+
+    #------------------------------------------------------------------
+    #------------------------------------------------------------------        
     def test_lookup_9_4_2_9_1_3_6_20_8_7_all_must_be_found(self):
         #arrange
         tree = BinaryTreeArray()
@@ -218,7 +213,8 @@ class Test_BinaryTreeArray:
         lookup_results = []
 
         for i in numbers:
-            lookup_results.append( tree.lookup(i) )
+            temp = tree.lookup(i)
+            lookup_results.append( temp.value if temp is not None else None )              
 
         #assert
         for i in range(len(numbers)):
