73755a5876aae63007f333c2380849d1d6cde5d4
Paulo <paulotcj@gmail.com>
2023-12-07 00:29:31 -0500

    .

--- python/algorithms/dijkstra_shortest_path/2_dijkstra_shortest_path.py
+++ python/algorithms/dijkstra_shortest_path/2_dijkstra_shortest_path.py
@@ -34,9 +34,9 @@ class MyGraph_Dijkstra_Path:
         # until all vertices visited    
     #-------------------------------------------------------------------------
     def __init__(self):
-        self.data = None
-        self.q = None
-        self.distance_table = None
+        self.data = {} #dict
+        self.q = [] # list
+        self.distance_table = {} #dict
     #-------------------------------------------------------------------------        
     #-------------------------------------------------------------------------
     def __add_vertex(self, node):
@@ -62,13 +62,18 @@ class MyGraph_Dijkstra_Path:
     #-------------------------------------------------------------------------
     def print(self):
         sorted_data = dict(sorted(self.data.items()))
+        return_result = []
         for k, v in sorted_data.items():
             temp_str = ""
             sorted_v = sorted(v , key= lambda x: x.node )
             for i in sorted_v:
-                temp_str += f"{i.node} (dist:{i.distance}), "
+                temp_str += f"{i.node} (dist:{i.edge_distance}), "
 
-            print(f"Node {k} - Connects to: {temp_str}")
+            temp_str = f"Node {k} - Connects to: {temp_str}"
+            return_result.append(temp_str)
+            print(temp_str)
+        
+        return return_result
     #-------------------------------------------------------------------------
     def __calculate_distance_table(self, current_node):
         current_node_dist = self.distance_table[current_node]
@@ -111,7 +116,7 @@ class MyGraph_Dijkstra_Path:
     def dijkstra(self, param_start):
         self.start = param_start
         self.q = []
-        self.data = {}
+        # self.data = {}
         self.distance_table = {}
 
         # current = None
@@ -122,6 +127,13 @@ class MyGraph_Dijkstra_Path:
             current = self.q.pop(0)
             #calculate distance table for current
             self.__calculate_distance_table(current) #this step is far too complicated so it is better to place it in a separate place
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def print_distance_table(self):
+        if self.distance_table == None: return
+
+        for k, v in self.distance_table:
+            print(f"Vertex: {k} - Shortest Distance: {v.shortest_distance} - Previous vertex: {v.prev} ")
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 class MyGraph_Dijkstra_Path_Test:
@@ -152,7 +164,20 @@ class MyGraph_Dijkstra_Path_Test:
         g.add_edge("B", "C", 5)
         g.add_edge("D", "E", 1)
         g.add_edge("E", "C", 5)
-        g.print()
+
+        expected_result = [
+            "Node A - Connects to: B (dist:6), D (dist:1), ",
+            "Node B - Connects to: A (dist:6), C (dist:5), D (dist:2), E (dist:2), ",
+            "Node C - Connects to: B (dist:5), E (dist:5), ",
+            "Node D - Connects to: A (dist:1), B (dist:2), E (dist:1), ",
+            "Node E - Connects to: B (dist:2), C (dist:5), D (dist:1), "
+        ]
+        result = g.print()
+        print("-------")
+        print(f"Does the summary results match the expected results? Answer: {result == expected_result}")
+        print("-------")
+        g.dijkstra(param_start="A")
+        # g.print_distance_table()
 
 
     #-------------------------------------------------------------------------
