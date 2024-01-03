d037541b43690f0ddaca1d678239720769d445e5
Paulo Costa <paulotcj@gmail.com>
2024-01-03 14:04:11 -0500

    .

--- python/algorithms/a_star_pathfinding/1_a_star_pathfinding.py
+++ python/algorithms/a_star_pathfinding/1_a_star_pathfinding.py
@@ -27,8 +27,10 @@ class A_Star_Pathfinding:
 
         distance = self.__calc_distance(self.element_locations[node1], self.element_locations[node2])
 
-        self.graph[node1][node2] = {'edge': distance, 'heuristic': float('infinity')}
-        self.graph[node2][node1] = {'edge': distance, 'heuristic': float('infinity')}
+        # self.graph[node1][node2] = {'edge': distance, 'heuristic': float('infinity')}
+        # self.graph[node2][node1] = {'edge': distance, 'heuristic': float('infinity')}
+        self.graph[node1][node2] = distance
+        self.graph[node2][node1] = distance
         # print(graph_dict)
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
@@ -44,41 +46,66 @@ class A_Star_Pathfinding:
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def dijkstra(self, start, end):
+        #modified Dijkstra's algorithm to use A* pathfinding
         graph = self.graph
         #--------------------
         self.__find_heuristic(end)
         #--------------------
-        # prep
-        queue = [(0, start)]
-        distances = {node: float('infinity') for node in graph} # creates a dictionary, the keys are the nodes in the graph, and the values are initially set to 'infinity'
-        distances[start] = 0
+        # prep:   heuristic                        dist  node
+        queue = [(self.heuristic_distances[start], 0,    start)]
+
+        distances = {node: [float('infinity'), self.heuristic_distances[node], float('infinity')] 
+                     for node in graph} # creates a dictionary, the keys are the nodes in the graph, and the values are initially set to 'infinity'
+        distances[start][0] = 0 
         path_table = {node: None for node in graph}
         #--------------------
         while queue:
-            inspecting_node_distance, inspecting_node = heapq.heappop(queue)
+            heuristic, inspecting_node_distance, inspecting_node = heapq.heappop(queue)
             #-----
-            saved_distance = distances[inspecting_node]
+            saved_distance = distances[inspecting_node][0]
             if inspecting_node_distance <= saved_distance: # even if its the same distance, we still want to process it, because we might find a shorter path down the road
                 self.calculate_new_distance(graph = graph, queue = queue, distances = distances,
                                     path_table = path_table, inspecting_node = inspecting_node,
-                                    inspecting_node_distance = inspecting_node_distance)
+                                    inspecting_node_distance = inspecting_node_distance,
+                                    end_node = end)
+                
+                if distances[end][0] < float('infinity'): #found a path
+                    break
             #-----
         #end of while
         #--------------------
         return distances, path_table
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def calculate_new_distance(self, graph, queue, distances, path_table, inspecting_node, inspecting_node_distance):
+    def calculate_new_distance(self, graph, queue, distances, path_table, inspecting_node, inspecting_node_distance, end_node):
         # the current distance is calculated from the perspective of the start node
         connections_to_curr_node = graph[inspecting_node]
-        for neighbor, neighbor_distances in connections_to_curr_node.items(): #from the perspective of the current node, we connect the current shortest distance of this node to its neighbors, and check if we found a new shorter path
-            new_distance = inspecting_node_distance + self.heuristic_distances[inspecting_node]  #new distance to the neighbor, perhapes we found a new shorter path?
-            if new_distance < distances[neighbor]:
-                distances[neighbor] = new_distance
+        for neighbor, neighbor_distance in connections_to_curr_node.items(): #from the perspective of the current node, we connect the current shortest distance of this node to its neighbors, and check if we found a new shorter path
+            
+            new_distance = inspecting_node_distance + neighbor_distance #new distance to the neighbor, perhapes we found a new shorter path?
+
+            if new_distance < distances[neighbor][0]:
+                distances[neighbor][0] = new_distance
+                distances[neighbor][2] = new_distance + self.heuristic_distances[neighbor]
                 path_table[neighbor] = inspecting_node
-                heapq.heappush(queue, (new_distance, neighbor))
-        
-    #-------------------------------------------------------------------------    
+                temp = self.heuristic_distances[neighbor] + new_distance
+                heapq.heappush(queue, (temp, new_distance, neighbor))
+
+                if neighbor == end_node: 
+                    print('hi')
+                    return
+                
+       
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def shortest_path(self, start, end):
+        distances, previous_nodes = self.dijkstra(start,end)
+        path = []
+        while end is not None:
+            path.append([end, distances[end][0]])
+            end = previous_nodes[end]
+        return path[::-1]               
+    #-------------------------------------------------------------------------
 
 
 #-------------------------------------------------------------------------
@@ -92,7 +119,23 @@ x.add_edge("A", "B")
 x.add_edge("A", "C")
 x.add_edge("B", "C")
 
-x.dijkstra("A", "C")
+print(x.shortest_path("A", "C"))
+
+# print('----------------------------------')
+
+# graph = {}
+# element_locations = { 'A': [1,1], 'B': [1,7] , 'C' : [7,7], 'D': [1,8] }
+
+
+# x = A_Star_Pathfinding(graph=graph, element_locations=element_locations)
+
+# x.add_edge("A", "D")
+# x.add_edge("A", "B")
+# x.add_edge("B", "C")
+# x.add_edge("D", "C")
+
+# print(x.shortest_path("A", "C"))
+
 
 print('hi')
 
