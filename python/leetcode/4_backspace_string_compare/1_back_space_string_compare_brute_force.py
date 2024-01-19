702af6bad6fb090791ed6510a7a05ea8999eeace
Paulo Costa <paulotcj@gmail.com>
2024-01-19 13:01:45 -0500

    .

---
+++ python/leetcode/4_backspace_string_compare/1_back_space_string_compare_brute_force.py
@@ -0,0 +1,32 @@
+class Solution:
+    #-------------------------------------------------------------------------
+    def buildString(self, s:str) -> str:
+        stack = []
+        #------
+        for c in s:
+            #------
+            if c != '#':
+                stack.append(c)
+            else:
+                if stack:
+                    stack.pop()
+            #------
+        #------
+        return ''.join(stack)
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        new_s = self.buildString(s)
+        new_t = self.buildString(t)
+
+        return new_s == new_t
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+    
+sol = Solution()
+s = "a#c"
+t = "b"
+expected = False
+result = sol.backspaceCompare(s,t)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
