e0959e84fafec1fa8caf02341f747545fd2f558b
Paulo Costa <paulotcj@gmail.com>
2024-01-20 10:15:43 -0500

    .

---
+++ python/leetcode/5_longest_substring_without_repeating_characters/3_longest_substring_without_repeating_characters_optimal.py
@@ -0,0 +1,65 @@
+#problem: https://leetcode.com/problems/backspace-string-compare/description/
+
+from typing import List, Dict, Tuple
+
+class Solution:
+
+    #-------------------------------------------------------------------------
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        low_idx = max_len = 0
+        seen = {}
+
+        for high_idx, high_v in enumerate(s):
+
+            if seen.get(high_v, -1) >= low_idx:  # -1 is the default value if 'high_v' is not found
+                low_idx = seen[high_v] + 1
+
+            max_len = max(max_len, high_idx - low_idx + 1)
+            seen[high_v] = high_idx
+
+        return max_len
+    #-------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------
+    
+print('----------------------------')
+sol = Solution()
+s = "tmmzuxt"
+expected = 5
+result = sol.lengthOfLongestSubstring(s)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
+
+
+# print('----------------------------')
+# sol = Solution()
+# s = "abcabcbb"
+# expected = 3
+# result = sol.lengthOfLongestSubstring(s)
+# print(f'result: {result}')
+# print(f'Is the result correct? { result == expected}')
+
+
+# print('----------------------------')
+# sol = Solution()
+# s = "bbbbb"
+# expected = 1
+# result = sol.lengthOfLongestSubstring(s)
+# print(f'result: {result}')
+# print(f'Is the result correct? { result == expected}')
+
+
+# print('----------------------------')
+# sol = Solution()
+# s = "pwwkew"
+# expected = 3
+# result = sol.lengthOfLongestSubstring(s)
+# print(f'result: {result}')
+# print(f'Is the result correct? { result == expected}')
+
+
+
+
+        
+        
