89d8d4468c7e771f7f4dd542954c1425b5073661
Paulo <paulotcj@gmail.com>
2024-01-30 15:46:29 -0500

    .

--- python/leetcode/10_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
+++ python/leetcode/10_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
@@ -1,9 +1,6 @@
 #problem: https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
-
 #-------------------------------------------------------------------------
-from typing import Optional
-
-
+from typing import Optional, List
 class Node:
     #-------------------------------------------------------------------------
     def __init__(self, val, prev, next, child):
@@ -13,6 +10,23 @@ class Node:
         self.child = child
         #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+class ProcessList:
+    def CreateFromArray(self, arr):
+        nodes_list : List(Node) = []
+        #-------
+        for i,v in enumerate(arr):
+            if v :
+                temp : Node = Node(v, None, None, None)
+                nodes_list.append(temp)
+            else:
+                nodes_list.append(None)
+        #-------
+        
+
+
+#-------------------------------------------------------------------------
+        
 
 
 class Solution:
