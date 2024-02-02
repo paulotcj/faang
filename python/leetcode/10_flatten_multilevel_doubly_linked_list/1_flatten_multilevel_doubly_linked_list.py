38ee2ca472cb8da1eb4c476a4c5407b7a93f1e45
Paulo <paulotcj@gmail.com>
2024-02-02 18:23:44 -0500

    .

--- python/leetcode/10_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
+++ python/leetcode/10_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
@@ -13,7 +13,7 @@ class Node:
 #-------------------------------------------------------------------------
 class ProcessList:
     #-------------------------------------------------------------------------
-    def CreateFromArray(self, arr):
+    def CreateFromArray(arr: List[int]):
         nodes_list : List(Node) = []
         #---------------
         for i,v in enumerate(arr):
@@ -61,20 +61,94 @@ class ProcessList:
         #---------------
             
         return nodes_list[0] if len(nodes_list) > 0 else None
-
     #-------------------------------------------------------------------------
-        
+    #-------------------------------------------------------------------------
+    def print_linked_list (head: Node) -> List[int]:
+        curr : Node = head
+        list : List[int] = []
+        while curr != None:
+            list.append(curr.val)
+            curr = curr.next
 
-
+        print(list)
+        return list 
+    #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
-        
-
-
+#-------------------------------------------------------------------------
 class Solution:
+    #-------------------------------------------------------------------------
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        pass
+        curr: Node = head
+        list_nodes : List[Node] = []
+        stack_pending_nodes : List[Node] = []
+
+        #-------------
+        while curr:
+            if curr.val == 12:
+                print('here')
+
+            list_nodes.append(curr)
+
+            if curr.child: #if the node has a child start processing the child's list
+                stack_pending_nodes.append(curr)
+                curr = curr.child
+                continue
+
+            curr = curr.next
+            while curr is None and len(stack_pending_nodes) > 0:
+                curr = stack_pending_nodes.pop().next
+        #-------------
+                
+        #-------------
+        for i, v in enumerate(list_nodes):
+            #curr - no need to change
+            #prev
+            if i > 0:
+                list_nodes[i].prev = list_nodes[i-1]
+            #next
+            if i < (len(list_nodes) - 1): #must be less: eg: len = 10, len -1 = 9, so i must be less or equal to 8
+                list_nodes[i].next = list_nodes[i+1]
+            #child
+            list_nodes[i].child = None
+        #-------------
+
+        return head
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 
 
+print('----------------------------')
+sol = Solution()
+arr = [1,2,3,4,5,6,None,None,None,7,8,None,None,11,12]
+# 1,2,3,4,5,6
+#     7,8
+#       11,12
+expected = [1,2,3,7,8,11,12,4,5,6]
+# error generated: [1,2,3,7,8,11,12]
+head = ProcessList.CreateFromArray(arr)
+
+
+head = sol.flatten(head)
+result = ProcessList.print_linked_list(head)
+print(f'result  : {result}')
+print(f'expected: {expected}')
+print(f'Is the result correct? { result == expected}')
+exit()
+
+print('----------------------------')
+sol = Solution()
 arr = [ 1, 2, 3, 4, 5, 6, None, None, None, 7, 8, 9, 10, None, None, 11, 12 ]
-head = ProcessList().CreateFromArray(arr)
+
+expected = [1,2,3,7,8,11,12,9,10,4,5,6]
+head = ProcessList.CreateFromArray(arr)
+
+
+head = sol.flatten(head)
+result = ProcessList.print_linked_list(head)
+print(f'result  : {result}')
+print(f'expected: {expected}')
+print(f'Is the result correct? { result == expected}')
+# exit()
+
+
 print('hi')
