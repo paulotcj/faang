4d26c5bd4ed1a4248088943c8c030a03352c5be1
Paulo Costa <paulotcj@gmail.com>
2024-02-03 01:32:11 -0500

    .

--- python/leetcode/11_Linked_List_Cycle_II/2_Linked_List_Cycle_II.py
+++ python/leetcode/11_Linked_List_Cycle_II/2_Linked_List_Cycle_II.py
@@ -34,27 +34,62 @@ class ProcessList:
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
+    # Using Floy's Tortoise and Hare algorithm - Cycle Detection
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        di_temp : List[ListNode] = {}
-        temp : ListNode = head
+        meeting_point : ListNode = self.find_meeting_point(head)
+        if meeting_point is None:
+            return None
 
-        while temp:
-            if temp in di_temp:
-                return temp
-            else:
-                di_temp[temp] = True
+        start_of_cycle : ListNode = self.find_start_of_cycle(head, meeting_point)
 
-            temp = temp.next
+        return start_of_cycle
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def find_start_of_cycle(self, head: ListNode, meeting_point: ListNode) -> ListNode:
+        start : ListNode = head
+        mp_temp : ListNode = meeting_point
+        while True:
+            if start == mp_temp:
+                return start
+            start = start.next
+            mp_temp = mp_temp.next
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def find_meeting_point(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None or head.next.next is None:
+            return None
+        
+        tortoise : ListNode = head.next
+        hare : ListNode = head.next.next
+
+        while hare and hare.next and hare.next.next:
+            if tortoise == hare:
+                return tortoise
+
+            tortoise = tortoise.next
+            hare = hare.next.next
 
         return None
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
-    
+
+print('----------------------------')
+arr = [1,2,3,4,5,6,7,8]
+pos = 2
+head = ProcessList.create_from_array(arr, pos)
+
+sol = Solution()
+result = sol.detectCycle(head)
+exit()
+print('hi')    
 
 print('----------------------------')
 arr = [3,2,0,-4]
 pos = 1
 head = ProcessList.create_from_array(arr, pos)
 
+sol = Solution()
+result = sol.detectCycle(head)
+exit()
 print('hi')
 
