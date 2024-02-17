38451237c7d15a506d68f313471e33857ab92613
Paulo Costa <paulotcj@gmail.com>
2024-02-17 13:53:00 -0500

    .

---
+++ python/leetcode/13_Minimum_Remove_to_Make_Valid_Parentheses/1_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -0,0 +1,3 @@
+#problem: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/description/
+#-------------------------------------------------------------------------
+from typing import List, Dict
