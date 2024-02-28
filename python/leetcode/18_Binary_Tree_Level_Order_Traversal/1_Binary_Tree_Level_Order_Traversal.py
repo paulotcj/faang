45c61abe976d406922e6a9c83c11aac24f966cb9
Paulo <paulotcj@gmail.com>
2024-02-28 18:42:19 -0500

    .

--- python/leetcode/18_Binary_Tree_Level_Order_Traversal/1_Binary_Tree_Level_Order_Traversal.py
+++ python/leetcode/18_Binary_Tree_Level_Order_Traversal/1_Binary_Tree_Level_Order_Traversal.py
@@ -1,3 +1,4 @@
+#problem: https://leetcode.com/problems/binary-tree-level-order-traversal
 from typing import List, Optional, Dict
 # Definition for a binary tree node.
 
@@ -16,24 +17,32 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root is None: return []
 
-        stack : List[ TreeNode ] = [ root ]
+        stack : List[ List[TreeNode, int] ] = [ [root,1] ]
+        return_list : List[List[int]] = []
+        #---
+        temp_l : List[TreeNode, int]
+        current = TreeNode
+        current_level : int
 
-        return_list = List[List[int]] = []
-        current_level_list : List[int] = []
-        level_identifier : TreeNode = stack[0]
-        current: TreeNode
-
-
+        #-------------
         while stack:
-            current  = stack.pop()
+            temp_l  = stack.pop()
+            current = temp_l[0]
+            current_level = temp_l[1]
 
+            #---
+            if len(return_list) < current_level: #new level needs to be added
+                return_list.append([current.val])
+            else:
+                return_list[current_level-1].append(current.val)
+            #---
+            if current.right: #the right side needs to be added first, because we will push to the stack and then when we pop this will be the last
+                stack.append([current.right,current_level+1])
             if current.left:
-                stack.append(current.left)
-            if current.right:
-                stack.append(current.right)
-
-
-
-
+                stack.append([current.left, current_level+1])
+            #---
+        #-------------
+                
+        return return_list
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
