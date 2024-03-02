621943f7a38d075b3688bf484b49cc13d24348c1
Paulo Costa <paulotcj@gmail.com>
2024-03-01 20:41:49 -0500

    .

--- python/leetcode/18_Binary_Tree_Level_Order_Traversal/1_Binary_Tree_Level_Order_Traversal.py
+++ python/leetcode/18_Binary_Tree_Level_Order_Traversal/1_Binary_Tree_Level_Order_Traversal.py
@@ -3,6 +3,22 @@ from typing import List, Optional, Dict
 # Definition for a binary tree node.
 
 #-------------------------------------------------------------------------
+class CreateTree:
+    #-------------------------------------------------------------------------
+    def create_tree():
+        root = TreeNode(3)
+        
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+
+        root.left.left = TreeNode(1)
+        root.left.right = TreeNode(4)
+        root.right.left = TreeNode(15)
+        root.right.right = TreeNode(7)
+        
+        return root
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class TreeNode:
     #-------------------------------------------------------------------------
     def __init__(self, val=0, left=None, right=None):
@@ -14,7 +30,7 @@ class TreeNode:
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def levelOrder_original(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root is None: return []
 
         stack : List[ List[TreeNode, int] ] = [ [root,1] ]
@@ -45,4 +61,43 @@ class Solution:
                 
         return return_list
     #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None: return []
+
+        queue : List[int] = [root]
+        count_down_level = 1
+        return_list : List[ List[int] ] = [[]]
+
+        #-------------
+        while queue:
+            current = queue.pop(0)
+            count_down_level -= 1
+            return_list[-1].append(current.val)
+
+
+            if current.left:
+                queue.append(current.left)       
+            if current.right:
+                queue.append(current.right)
+         
+
+            if count_down_level == 0:
+                count_down_level = len(queue)
+                return_list.append([])
+        #-------------
+                
+        return_list.pop() #remove the last empty list
+                
+        return return_list
+
+    #-------------------------------------------------------------------------    
 #-------------------------------------------------------------------------
+    
+sol = Solution()
+root = CreateTree.create_tree()
+result = sol.levelOrder(root)
+expected = [[3], [9, 20], [1, 4, 15, 7]]
+print(f'result  : {result}')
+print(f'expected: {expected}')
+print(f'result == expected: {result == expected}')
