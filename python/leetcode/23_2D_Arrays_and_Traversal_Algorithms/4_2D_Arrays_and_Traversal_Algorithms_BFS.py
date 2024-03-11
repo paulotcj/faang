156a7508cb6c7c153786f6964a21aa7b5947498a
Paulo Costa <paulotcj@gmail.com>
2024-03-11 00:57:28 -0400

    .

--- python/leetcode/23_2D_Arrays_and_Traversal_Algorithms/4_2D_Arrays_and_Traversal_Algorithms_BFS.py
+++ python/leetcode/23_2D_Arrays_and_Traversal_Algorithms/4_2D_Arrays_and_Traversal_Algorithms_BFS.py
@@ -3,11 +3,17 @@
 class create_data_structures:
     #-------------------------------------------------------------------------
     def create_matrix():
+        # matrix = [
+        #     [ 1,  2,  3,  4,  5  ],
+        #     [ 6,  7,  8,  9,  10 ],
+        #     [ 11, 12, 13, 14, 15 ],
+        #     [ 16, 17, 18, 19, 20 ]
+        # ]
         matrix = [
-            [ 1,  2,  3,  4,  5  ],
-            [ 6,  7,  8,  9,  10 ],
-            [ 11, 12, 13, 14, 15 ],
-            [ 16, 17, 18, 19, 20 ]
+            [  1,  2,  3,  4 ],
+            [  5,  6,  7,  8 ],
+            [  9, 10, 11, 12 ],
+            [ 13, 14, 15, 16 ]
         ]
         return matrix
     #-------------------------------------------------------------------------
@@ -35,30 +41,34 @@ class MatrixDFS:
         return directions
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def traversal_dfs(self, matrix, row=0, col=0):
+    def traversal_bfs(self, matrix, row=0, col=0):
         self.seen = self.create_seen_matrix(matrix)  # this is dependent on the matrix
         queue = [(row, col)]
 
+        #--------------------
         while queue:
             row, col = queue.pop(0)
             if row < 0 or row >= len(matrix): continue
-            if col < 0 or col >= len(matrix[0]) : continue 
-            if self.seen[row][col] : continue
+            if col < 0 or col >= len(matrix[0]): continue
+            if self.seen[row][col]: continue
 
             self.seen[row][col] = True
             self.path_explored.append(matrix[row][col])
-        
-            queue = [ [row + x[0], col + x[1]] for x in self.directions ]
 
-        return self.path_explored    
+            for dir in self.directions:
+                queue.append((row + dir[0], col + dir[1]))
+            
+        #--------------------
+        return self.path_explored
+
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 
-#expected result: (20) [1, 2, 3, 4, 5, 10, 15, 20, 19, 14, 9, 8, 13, 18, 17, 12, 7, 6, 11, 16]
+#expected result: (16) [1, 2, 5, 3, 6, 9, 4, 7, 10, 13, 8, 11, 14, 12, 15, 16]
 x = MatrixDFS()
 matrix = create_data_structures.create_matrix()
-result = x.traversal_dfs(matrix)
-expected = [1, 2, 3, 4, 5, 10, 15, 20, 19, 14, 9, 8, 13, 18, 17, 12, 7, 6, 11, 16]
+result = x.traversal_bfs(matrix)
+expected = [1, 2, 5, 3, 6, 9, 4, 7, 10, 13, 8, 11, 14, 12, 15, 16]
 print(f'result: {result}')
 print(f'expected: {expected}')
 print(f'Is the result what was expected?: {result == expected}')
