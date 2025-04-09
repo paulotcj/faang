eb1f5d38d17770b71515c24ab3273b50ae660ebb
Paulo <paulotcj@gmail.com>
2025-04-09 19:25:31 -0400

    .

--- python/udemy_leetcode/27_Intro_to_graphs/1_Adjacency_List_BFS.py
+++ python/udemy_leetcode/27_Intro_to_graphs/1_Adjacency_List_BFS.py
@@ -1,17 +1,18 @@
+from typing import List
+
 #-------------------------------------------------------------------------
-def traversal_bfs(graph):
-    queue = [0]
-    values = []
-    seen = {}
+def traversal_bfs(graph , start_idx) -> List[int]:
+    queue         = [start_idx]
+    explored_path = []
+    seen          = {}
 
     #---------------------------
     while queue:
         vertex = queue.pop(0)
-        values.append(vertex)
+        explored_path.append(vertex)
         seen[vertex] = True
 
-        connections = graph[vertex]
-
+        connections = graph[vertex] # get the vertex's connections
         #---------------------------
         for connection in connections:
             if connection not in seen:
@@ -20,20 +21,20 @@ def traversal_bfs(graph):
 
     #---------------------------
 
-    return values
+    return explored_path
 #-------------------------------------------------------------------------
 
 adjacency_list = [
-    [1, 3],
-    [0],
-    [3, 8],
-    [0, 2, 4, 5],
-    [3, 6],
-    [3],
-    [4, 7],
-    [6],
-    [2]
+    [1, 3],         #0 - start idx
+    [0],            #1
+    [3, 8],         #2
+    [0, 2, 4, 5],   #3
+    [3, 6],         #4
+    [3],            #5
+    [4, 7],         #6
+    [6],            #7
+    [2]             #
 ]
 
-values = traversal_bfs(adjacency_list)
+values = traversal_bfs(graph = adjacency_list, start_idx=0)
 print(values)
