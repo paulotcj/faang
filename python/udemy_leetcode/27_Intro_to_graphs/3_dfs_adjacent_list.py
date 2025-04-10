94d15a56daa9575823aeda90d79acc78992e916d
Paulo <paulotcj@gmail.com>
2025-04-10 03:27:23 -0400

    .

--- python/udemy_leetcode/27_Intro_to_graphs/3_dfs_adjacent_list.py
+++ python/udemy_leetcode/27_Intro_to_graphs/3_dfs_adjacent_list.py
@@ -21,6 +21,7 @@ def dfs_adj_list(current_idx, graph, p_visited_path, seen):
     p_visited_path.append(current_idx)
     seen[current_idx] = True
 
+    #---------------------------
     connections = graph[current_idx]
     for cnn in connections:
         if cnn not in seen:
@@ -30,29 +31,67 @@ def dfs_adj_list(current_idx, graph, p_visited_path, seen):
                 p_visited_path  = p_visited_path,
                 seen            = seen
             )
+    #---------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 def dfs_adj_list_iterative(start_idx, graph):
+    stack = [start_idx]
     visited_path = []
     seen = {}
-    stack = [start_idx]
 
+    #---------------------------
     while stack:
         current = stack.pop()
         if current not in seen:
             visited_path.append(current)
             seen[current] = True
 
-            # Add neighbors to the stack in reverse order to maintain the same order as recursion
-            for neighbor in reversed(graph[current]):
-                if neighbor not in seen:
-                    stack.append(neighbor)
+            #---------------------------
+            '''Add neighbors to the stack in reverse order to maintain the same order as recursion
+             a little further explanation here. Consider the connections to the node 0: [1, 3]
+             we should visit first 1 and then 3. If we put then in the stack as they are the stack
+             would be [1,3], and we we pop we would get 3 first instead of 1. Therefore we need to
+             reverse the array to [3,1], so when we pop the stack 1 would be the first to come out.
+             And we can't use a queue because, first this is not a queue, and second we need to backtrack, and more
+             intuitively, the previous implementation of recursion tells us that was a stack
+             abstraction.
+            '''
+            rev_conns = reversed(graph[current])
+            for conn in rev_conns:
+                if conn not in seen:
+                    stack.append(conn)
+            #---------------------------
+    #---------------------------
+
+    return visited_path
+#-------------------------------------------------------------------------
+from collections import deque
+from typing import List
+#-------------------------------------------------------------------------
+def dfs_adj_list_iterative_2(graph, start_idx):
+    stack = deque([start_idx])
+    visited_path = []
+    seen = set()
+
+    #---------------------------
+    while stack:
+        curr = stack.pop()
+        if curr not in seen:
+            visited_path.append(curr)
+            seen.add(curr)
+
+            #---------------------------
+            rev_conns = reversed( graph[curr] ) #this is not sorted but reversed from whatever order it was provided
+            for cn in rev_conns:
+                if cn not in seen:
+                    stack.append(cn)
+            #---------------------------
+    #---------------------------
 
     return visited_path
 #-------------------------------------------------------------------------
 
-from collections import deque
-from typing import List
+
 
 adjacency_list = [
     [1, 3],         # 0
@@ -87,8 +126,15 @@ print(global_visited_path)
 
 
 
-iterative_visited_path = dfs_adj_list_iterative(0, adjacency_list)
-print(iterative_visited_path)
+result = dfs_adj_list_iterative(start_idx = 0, graph = adjacency_list)
+print(result)
+
+
+result = dfs_adj_list_iterative_2(start_idx = 0, graph = adjacency_list)
+print(result)
+
+
+
 
 # (9) [0, 1, 3, 2, 8, 4, 6, 7, 5]
 #     [0, 1, 3, 2, 8, 4, 6, 7, 5]
