46127ea9d22e847db509f65573b42ec955dc03ea
Paulo <Costa>
2025-04-16 14:06:26 -0400

    .

--- python/udemy_leetcode/28_time_needed_to_inform_all_employees/5_time_needed_to_inform_all_employees_BFS.py
+++ python/udemy_leetcode/28_time_needed_to_inform_all_employees/5_time_needed_to_inform_all_employees_BFS.py
@@ -1,27 +1,44 @@
 from collections import defaultdict, deque
-from typing import List, Deque, Tuple
+from typing import List, Deque, Tuple, Dict
 
 
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        subordinates = defaultdict(list)
+        subordinates : Dict[int, List[int]] = defaultdict(list)
 
         #---------------------------
         for employee_id in range(n):
-            emp_manager = manager[employee_id]
+            emp_manager : int = manager[employee_id]
             if emp_manager == -1: continue
 
             subordinates[emp_manager].append( employee_id )
         #---------------------------
 
         queue : Deque[Tuple[int,int]] = deque([(headID, 0)])
-        max_time = 0
-
+        max_time : int = 0
+        
+        #---------------------------
+        while queue:
+            current_emp_id , time_taken_upstream = queue.pop()
+            max_time : int = max(max_time, time_taken_upstream)
+            
+            #---------------------------
+            #explore child nodes BFS style
+            subordinates_slice : List[int] = subordinates[current_emp_id]
+            for subordinate_id in subordinates_slice:
+                manager_inform_time  : int = informTime[current_emp_id]
+                
+                queue.append( (subordinate_id, manager_inform_time + time_taken_upstream) )
+            #---------------------------
+                
+        #---------------------------
+        
+        return max_time
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+    def numOfMinutes_old(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         # Build an adjacency list to represent the tree structure
         subordinates = defaultdict(list)
         
