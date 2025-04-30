82436fded0ce7b71781ff6ebb5adf895ab13ef99
Paulo <Costa>
2025-04-30 02:54:42 -0400

    .

--- python/udemy_leetcode/30_Network_time_delay/3_network_time_delay_dijkstra_2.py
+++ python/udemy_leetcode/30_Network_time_delay/3_network_time_delay_dijkstra_2.py
@@ -1,37 +1,49 @@
 # https://leetcode.com/problems/network-delay-time/description/
 
-from heapq import heappop, heappush
+# from heapq import heappop, heappush
+
+import heapq
 from collections import defaultdict
-from typing import List
+from typing import List, Tuple, Dict
 
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
+    
+    #-------------------------------------------------------------------------
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        pass
+    #-------------------------------------------------------------------------
+    def networkDelayTime_old(self, times: List[List[int]], n: int, k: int) -> int:
         # Build the graph as an adjacency list
-        graph = defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v, w))
+        graph : defaultdict[ int, List[ Tuple[int,int] ] ] = defaultdict(list) # any key not present, the default value is a list
+        for from_node, to_node, time_needed in times:
+            graph[from_node].append((to_node, time_needed))
         
         # Min-heap to store (time, node)
-        heap = [(0, k)]
-        # Dictionary to store the shortest time to reach each node
-        shortest_time = {}
+        heap : List[ Tuple[int,int] ] = [(0, k)]
         
+        # Dictionary to store the shortest time to reach each node
+        shortest_time : Dict[int, int] = {}
+        
+        #-----------------------------------
         while heap:
-            time, node = heappop(heap)
+            current_time, current_vertex = heapq.heappop(heap)
             
             # If the node is already visited, skip it
-            if node in shortest_time:
+            if current_vertex in shortest_time:
                 continue
             
             # Record the shortest time to reach this node
-            shortest_time[node] = time
+            shortest_time[current_vertex] = current_time
             
+            #-----------------------------------
             # Explore neighbors
-            for neighbor, weight in graph[node]:
-                if neighbor not in shortest_time:
-                    heappush(heap, (time + weight, neighbor))
+            for neigh_vertex, neigh_time_needed in graph[current_vertex]:
+                if neigh_vertex not in shortest_time:
+                    heapq.heappush(heap, (current_time + neigh_time_needed, neigh_vertex))
+            #-----------------------------------
+        #-----------------------------------
         
         # If all nodes are reached, return the maximum time; otherwise, return -1
         return max(shortest_time.values()) if len(shortest_time) == n else -1
