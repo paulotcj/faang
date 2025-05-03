9f5258637777f5a946b59a23fdb530408bad5d75
Paulo <paulotcj@gmail.com>
2025-05-03 15:00:00 +0500

    .

--- python/leetcode/092_Linked_List_reverse_linked_list_2/2_reverse_linked_list.py
+++ python/leetcode/092_Linked_List_reverse_linked_list_2/2_reverse_linked_list.py
@@ -73,6 +73,7 @@ class Solution:
             left_target.next = None
             
         return head
+    
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def get_obj_markers( self , head : ListNode , left_idx : int , right_idx : int ) -> List[ListNode] :
