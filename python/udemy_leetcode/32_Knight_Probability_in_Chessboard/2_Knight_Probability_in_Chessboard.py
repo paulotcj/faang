2731b3c0e9f632152f7c0f180d2f477c9cfc1731
Paulo <Costa>
2025-05-07 13:30:00 -0400

    .

--- python/udemy_leetcode/32_Knight_Probability_in_Chessboard/2_Knight_Probability_in_Chessboard.py
+++ python/udemy_leetcode/32_Knight_Probability_in_Chessboard/2_Knight_Probability_in_Chessboard.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 # knight moves. It moves in a L pattern, 
 DIRECTIONS = [
     [-2 , -1], # 2 down one left
@@ -15,30 +17,37 @@ DIRECTIONS = [
 class Solution:
     #-------------------------------------------------------------------------
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        #-----------------------------------
-        # base cases
-        if row < 0 or row >= n or column < 0 or column >= n: return 0
+        stack : List[Tuple[int,int,int, float]] = [(row, column, k, 1.0)] #row, col, k, prob
+        result: float = 0.0
         
-        if k == 0: return 1
         #-----------------------------------
-        
-        accu_resul : float = 0
-
-        #-----------------------------------
-        for dir_row, dir_col in DIRECTIONS:
-            dir_row : int = dir_row + row
-            dir_col : int = dir_col + column
+        while stack:
+            while_row, while_col, moves_left, prob = stack.pop()
             
-            temp_result : float = self.knightProbability(n = n, k = k-1, row = dir_row, column = dir_col) / 8
-            accu_resul : float = accu_resul + temp_result
+            # If position is out of the board, ignore it
+            if while_row < 0 or while_row >= n or while_col < 0 or while_col >= n:
+                continue
+            
+            # If no moves left, add probability to result
+            if moves_left == 0:
+                result += prob
+            else:
+                # Push the next possible moves onto the stack
+                for dr, dc in DIRECTIONS:
+                    stack.append((while_row + dr, while_col + dc, moves_left - 1, prob / 8))
         #-----------------------------------
         
-        return accu_resul
-    #-------------------------------------------------------------------------
+        return result
+    #-------------------------------------------------------------------------    
 #-------------------------------------------------------------------------
 
 sol = Solution()
 
 result = sol.knightProbability(n = 3, k = 2, row = 0, column = 0)
 expected = 0.06250
+print(f'result: {result}, expected: {expected}, are they equal?: {abs(result - expected) < 1e-5}')
+
+print()
+result = sol.knightProbability(n = 8, k = 30, row = 6, column = 4)
+expected = 0.0 # at this point the solution takes too long and we don't know the answer
 print(f'result: {result}, expected: {expected}, are they equal?: {abs(result - expected) < 1e-5}')
