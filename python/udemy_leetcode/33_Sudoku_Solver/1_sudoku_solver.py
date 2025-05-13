85652ed604896d34b6d8feebb44aea04c0bd4484
Paulo <Costa>
2025-05-13 02:34:31 -0400

    .

--- python/udemy_leetcode/33_Sudoku_Solver/1_sudoku_solver.py
+++ python/udemy_leetcode/33_Sudoku_Solver/1_sudoku_solver.py
@@ -78,7 +78,6 @@ class Solution:
         
         blockRow = (row // 3) * 3 # e.g.: 4 // 3 = 1 -> 1 * 3 = 3
         blockCol = (col // 3) * 3 # e.g.: 5 // 3 = 1 -> 1 * 3 = 3
-        # blockCol = (col // 3)
         
         #-----------------------------------
         for i in range(self.board_width): # from 0 to 8
@@ -87,8 +86,28 @@ class Solution:
             if self.board[row][i] == c or self.board[i][col] == c: 
                 return False
             
-            ''' remember that i goes from 0 to 9 and that the row = 4 and col = 5 '''
+            ''' remember that i goes from 0 to 8 and that the row = 4 and col = 5, and the
+            curRow = 3 and curCol = 3 (3,3), so in the example below we would have:
             
+              curRow -> 3 + (0 // 3) = 3 + 0 = 3 ... 3 + (3 // 3) = 3 + 1 = 4 ...
+                3 + (6//3) = 3 + 2 = 5 ... 3 + (8//3) = 3 + 2 = 5
+              So we can see the values here go from 3 to 5 where for 
+              i(0) = 3, i(1) = 3, i(2) = 3, i(3) = 4, i(4) = 4, i(5) = 4, i(6) = 5, i(7) = 5, 
+              i(8) = 5
+                
+              curCol -> 3 + (0%3) = 3 + 0 = 3 ... 3 + (1%3) = 3 + 1 = 4 ... 3 + (2%3) = 3 + 2 = 5
+                3 + (3%3) = 3 + 0 = 3 ... 3 + (4%3) = 3 + 1 = 4 ... 3 + (6%3) = 3 + 0 = 3 ... 
+                3 + (8%3) = 3 + 2 = 5
+              And that here the values range from 3 to 5, where for
+              i(0) = 3, i(1) = 4, i(2) = 5, i(3) = 3, i(4) = 4, i(5) = 5, i(6) = 3, i(7) = 4, i(8) = 5 
+              
+            Now if we look at the coordinates together we have for:
+            i(0) = (3,3), i(1) = (3,4), i(2) = (3,5), 
+            i(3) = (4,3), i(4) = (4,4), i(5) = (4,5), 
+            i(6) = (5,3), i(7) = (5,4), i(8) = (5,5) 
+            
+            The exact elements from the block in question'''
+              
             curRow = blockRow + (i // 3)
             curCol = blockCol + (i % 3)   
             if self.board[curRow][curCol] == c:
