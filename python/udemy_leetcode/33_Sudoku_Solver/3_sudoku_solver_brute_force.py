41339f3640deeb1b529e9519bcc52f19fce3124a
Paulo <Costa>
2025-05-13 18:27:48 -0400

    .

--- python/udemy_leetcode/33_Sudoku_Solver/3_sudoku_solver_brute_force.py
+++ python/udemy_leetcode/33_Sudoku_Solver/3_sudoku_solver_brute_force.py
@@ -6,117 +6,78 @@ from typing import List
 class Solution:
     #-------------------------------------------------------------------------
     def solveSudoku(self, board: List[List[str]]) -> None:
-        self.board : List[List[str]] = board
-        self.board_width : int = len(board)
-        self.dfs()
+        n : int = len(board)
+        boxes = [{} for _ in range(n)]
+        rows = [{} for _ in range(n)]
+        cols = [{} for _ in range(n)]
+
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] != ".":
+                    boxId = self.getBoxId(r, c)
+                    val = board[r][c]
+                    boxes[boxId][val] = True
+                    rows[r][val] = True
+                    cols[c][val] = True
+
+        self.solveBacktrack(board, boxes, rows, cols, 0, 0)
+    #-------------------------------------------------------------------------    
+    #-------------------------------------------------------------------------
+    def getBoxId(self, row, col):
+        rowVal = (row // 3) * 3
+        colVal = (col // 3)
+        return rowVal + colVal
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def dfs(self) -> bool:
-        
-        # we go for every cell on the board, taking row first and then column
-        #-----------------------------------
-        for for_row in range(self.board_width):
-            for for_col in range(self.board_width):
-                if self.board[for_row][for_col] != "." : continue # a number is already placed there
-                
-                # try every number from 0 to 9
-                #-----------------------------------
-                for i in range(1, 10): # 10 is not inclusive
-                    c : str = str(i)
-                    
-                    #-----------------------------------
-                    if self.is_valid( row = for_row, col = for_col, c = c) == True:
-                        self.board[for_row][for_col] = c
-                        
-                        # now we have to continue to look for next steps in the solution
-                        if self.dfs() == True : return True
-                    #-----------------------------------
-                    # we can't return false here just because the self.is_valid returned false
-                    # as we havent explored all the possibilities with all the numbers
-                #-----------------------------------
-                
-                self.board[for_row][for_col] = "."
-                return False # now after the for loop we can dafely return false as we explored all numbers      
-        #-----------------------------------
-        
-        # now after all rows and cols were explored, if we havent run into a invalid situation
-        #   then we found a valid solution
-        return True
+    def isValid(self, box, row, col, num):
+        if box.get(num) or row.get(num) or col.get(num):
+            return False
+        else:
+            return True
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def is_valid(self, row : int, col : int , c : str) -> bool:
-        ''' blocks:
-            | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
-            -------------------------------------
-          0 |   |   |   |   |   |   |   |   |   |
-          1 |   |   |   |   |   |   |   |   |   |
-          2 |   |   |   |   |   |   |   |   |   |
-          3 |   |   |   |   |   |   |   |   |   |
-          4 |   |   |   |   |   |   |   |   |   |
-          5 |   |   |   |   |   |   |   |   |   |
-          6 |   |   |   |   |   |   |   |   |   |
-          7 |   |   |   |   |   |   |   |   |   |
-          8 |   |   |   |   |   |   |   |   |   |
-          
-            | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
-            -------------------------------------
-          0 |           |           |           |
-          1 |     0     |     1     |     2     |
-          2 |____________________________________
-          3 |           |           |           |
-          4 |     3     |     4     |     5     |
-          5 |____________________________________
-          6 |           |           |           |
-          7 |     6     |     7     |      8    |
-          8 |____________________________________
-        
-        consider row 4 col 5 i supposed to be block 4, and block 4 starts at row 3 col 3
-        so (3,3)
-        '''
-                
-        starting_block_row = (row//3) * 3 # e.g.: 4 // 3 = 1 -> 1 * 3 = 3
-        starting_block_col = (col//3) * 3 # e.g.: 5 // 3 = 1 -> 1 * 3 = 3
-        
-        '''We got 'c', now we have to check for all the 9 positions in the current row, all the
-        9 positions in the current column, and all the 9 positions in the current block'''
-        #-----------------------------------
-        for i in range(self.board_width): # loop 9 times from 0 to 8
-            # check if this number exists in the row or col
-            if self.board[row][i] == c or self.board[i][col] == c :
-                return False
-            
-            ''' remember that i goes from 0 to 8 and that the row = 4 and col = 5, and the
-            curRow = 3 and curCol = 3 (3,3), so in the example below we would have:
-            
-              curRow -> 3 + (0 // 3) = 3 + 0 = 3 ... 3 + (3 // 3) = 3 + 1 = 4 ...
-                3 + (6//3) = 3 + 2 = 5 ... 3 + (8//3) = 3 + 2 = 5
-              So we can see the values here go from 3 to 5 where for 
-              i(0) = 3, i(1) = 3, i(2) = 3, i(3) = 4, i(4) = 4, i(5) = 4, i(6) = 5, i(7) = 5, 
-              i(8) = 5
-                
-              curCol -> 3 + (0%3) = 3 + 0 = 3 ... 3 + (1%3) = 3 + 1 = 4 ... 3 + (2%3) = 3 + 2 = 5
-                3 + (3%3) = 3 + 0 = 3 ... 3 + (4%3) = 3 + 1 = 4 ... 3 + (6%3) = 3 + 0 = 3 ... 
-                3 + (8%3) = 3 + 2 = 5
-              And that here the values range from 3 to 5, where for
-              i(0) = 3, i(1) = 4, i(2) = 5, i(3) = 3, i(4) = 4, i(5) = 5, i(6) = 3, i(7) = 4, i(8) = 5 
-              
-            Now if we look at the coordinates together we have for:
-            i(0) = (3,3), i(1) = (3,4), i(2) = (3,5), 
-            i(3) = (4,3), i(4) = (4,4), i(5) = (4,5), 
-            i(6) = (5,3), i(7) = (5,4), i(8) = (5,5) 
-            
-            The exact elements from the block in question'''
-            
-            probing_row_i = starting_block_row + ( i // 3) # the row only needs to change every 3rd 'i'th
-            probing_col_i = starting_block_col + (i % 3 )  # the column needs to change every 'i'th but resets every 3rd 'i'th
-            
-            if self.board[probing_row_i][probing_col_i] == c : 
-                return False
-                      
-        #-----------------------------------
-        
-        return True
+    def solveBacktrack(self, board, boxes, rows, cols, r, c):
+        if r == len(board) or c == len(board[0]):
+            return True
+        else:
+            if board[r][c] == ".":
+                for num in range(1, 10):
+                    numVal = str(num)
+                    board[r][c] = numVal
+
+                    boxId = self.getBoxId(r, c)
+                    box = boxes[boxId]
+                    row = rows[r]
+                    col = cols[c]
+
+                    if self.isValid(box, row, col, numVal):
+                        box[numVal] = True
+                        row[numVal] = True
+                        col[numVal] = True
+
+                        if c == len(board[0]) - 1:
+                            if self.solveBacktrack(board, boxes, rows, cols, r + 1, 0):
+                                return True
+                        else:
+                            if self.solveBacktrack(board, boxes, rows, cols, r, c + 1):
+                                return True
+
+                        del box[numVal]
+                        del row[numVal]
+                        del col[numVal]
+
+                    board[r][c] = "."
+            else:
+                if c == len(board[0]) - 1:
+                    if self.solveBacktrack(board, boxes, rows, cols, r + 1, 0):
+                        return True
+                else:
+                    if self.solveBacktrack(board, boxes, rows, cols, r, c + 1):
+                        return True
+
+        return False
     #-------------------------------------------------------------------------
+
 
 #-------------------------------------------------------------------------
 
