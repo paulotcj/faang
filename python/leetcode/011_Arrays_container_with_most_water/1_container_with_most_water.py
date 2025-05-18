4dc33a03450091633db5093cf689b24ec7c0f9c4
Paulo <Costa>
2025-05-17 23:26:24 -0400

    .

--- python/leetcode/011_Arrays_container_with_most_water/1_container_with_most_water.py
+++ python/leetcode/011_Arrays_container_with_most_water/1_container_with_most_water.py
@@ -1,4 +1,4 @@
-#original problem: https://leetcode.com/problems/container-with-most-water/
+# https://leetcode.com/problems/container-with-most-water/
 
 from typing import List
 
@@ -8,12 +8,14 @@ class Solution:
     def maxArea2(self, height: List[int]) -> int:
         max_area = 0
 
+        #-----------------------------------
         for i, v_i in enumerate(height):
             for j in range(i+1, len(height)):
                 v_j = height[j]
                 area = min(v_i, v_j) * (j-i)
                 max_area = max(max_area, area)
-        #---
+        #-----------------------------------
+
         return max_area
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
@@ -21,7 +23,8 @@ class Solution:
 
         left, right = 0, len(height)-1
         max_volume = 0 
-        #------
+
+        #-----------------------------------
         while left<right:
             new_volume = min(height[left],height[right]) * (right-left)
             max_volume = max(new_volume, max_volume)
@@ -29,15 +32,16 @@ class Solution:
                 left +=1
             else: 
                 right -=1
-        #------
-        
+        #-----------------------------------
+
         return max_volume    
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def maxArea(self, height: List[int]) -> int:
         left_i , right_i = 0, len(height)-1
         max_volume = 0
-        #------
+
+        #-----------------------------------
         while left_i < right_i:
             new_volume = min(height[left_i], height[right_i]) * (right_i - left_i)
             max_volume = max(max_volume, new_volume)
@@ -46,7 +50,8 @@ class Solution:
                 left_i += 1
             else:
                 right_i -= 1
-        #------
+        #-----------------------------------
+
         return max_volume
     #-------------------------------------------------------------------------
 
