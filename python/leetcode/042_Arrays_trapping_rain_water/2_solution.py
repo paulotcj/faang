c3f0733e94c04f1f44b7a37b32a95c7536629343
Paulo <Costa>
2025-05-18 21:34:45 -0400

    .

--- python/leetcode/042_Arrays_trapping_rain_water/2_solution.py
+++ python/leetcode/042_Arrays_trapping_rain_water/2_solution.py
@@ -3,24 +3,29 @@ from typing import List, Tuple
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def _find_first_left(self) -> Tuple[int,int]:
-        for i , v in enumerate(self.height):
-            if v > 0:
-                return (i, v)
+    def _find_first_left(self) -> Tuple[int,int]: # find the first occurence of a non zero height
+        for idx, val in enumerate(self.height):
+            if val > 0 : return (idx,val)
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def _find_first_right(self) -> Tuple[int,int]:
-        for i, v in enumerate(self.height[::-1]):
-            if v > 0:
-                return ((len(self.height) - 1) - i , v)
+        for idx, val in enumerate(self.height[::-1]): # loop backwards
+            if val > 0: #found the first non zero, so... a wall
+                idx_aux : int = (len(self.height) - 1) - idx # is this how many steps from the last idx?
+                ret_val : Tuple[int,int] = (idx_aux, val)
+                return ret_val
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def _handle_values(self, left_i: int, right_i: int) -> Tuple[int,int]:
-            left_v = self.height[left_i]
-            right_v = self.height[right_i]
-            self.max_left = max(self.max_left, left_v)
-            self.max_right = max(self.max_right, right_v)
-            return (left_v , right_v)
+    def _handle_values(self, left_i : int, right_i : int) -> Tuple[int,int]:
+        ''' get the values for left_i and right_i, and try to update the max
+        values for self.max_left and self.max_right'''
+        left_v : int = self.height[left_i]
+        right_v : int = self.height[right_i]
+        
+        self.max_left : int = max(self.max_left, left_v)    # i don't get it why we use those
+        self.max_right : int = max(self.max_right, right_v)
+        
+        return (left_v, right_v)
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def _handle_left(self, left_v: int) -> None:
@@ -35,14 +40,29 @@ class Solution:
             self.total_water += current_water
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    # def trap(self, height: list[int]) -> int:
+    def trap2(self, height: list[int]) -> int:
+        if len(height) < 3 : return 0 # you need at least a wall, a well, and a wall
+        
+        self.height : List[int] = height
+        self.total_water : int = 0
+        
+        left_i , self.max_left = self._find_first_left() # find the first occurence of a non zero height
+        right_i, self.max_right = self._find_first_right() # number of backwards steps from the non-zero value, and the height value
+        
+        #-----------------------------------
+        while left_i < right_i:
+            # get the values for left and right, but at the same time update max_left and
+            #   max_right if a new max value is found
+            left_v , right_v = self._handle_values(left_i = left_i, right_i = right_i)
+        #-----------------------------------
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def trap(self, height: list[int]) -> int:
-        if len(height) <= 2: return 0
+        if len(height) < 3: return 0 # you need at least a wall, a well, and a wall
         
         self.height : List[int] = height
         self.total_water: int = 0
+        
         left_i, self.max_left = self._find_first_left()
         right_i , self.max_right = self._find_first_right()
         
