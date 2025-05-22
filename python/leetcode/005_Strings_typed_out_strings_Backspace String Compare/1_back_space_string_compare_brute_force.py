14fa6f8cfa8f7b91ef754b89e60de1b47cea6462
Paulo <Costa>
2025-05-22 19:05:17 -0400

    .

--- python/leetcode/005_Strings_typed_out_strings_Backspace String Compare/1_back_space_string_compare_brute_force.py
+++ python/leetcode/005_Strings_typed_out_strings_Backspace String Compare/1_back_space_string_compare_brute_force.py
@@ -1,19 +1,9 @@
 #problem: https://leetcode.com/problems/backspace-string-compare/description/
+
+
+#-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def buildString(self, s:str) -> str:
-        stack = []
-        #------
-        for c in s:
-            #------
-            if c != '#':
-                stack.append(c)
-            else:
-                if stack:
-                    stack.pop()
-            #------
-        #------
-        return ''.join(stack)
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def backspaceCompare(self, s: str, t: str) -> bool:
@@ -21,9 +11,66 @@ class Solution:
         new_t = self.buildString(t)
 
         return new_s == new_t
+    #-------------------------------------------------------------------------    
+    #-------------------------------------------------------------------------
+    def buildString(self, s:str) -> str:
+        stack = []
+        #-----------------------------------
+        for c in s:
+            #----------
+            if c != '#':
+                stack.append(c)
+            else:
+                if stack:
+                    stack.pop()
+            #----------
+        #-----------------------------------
+        return ''.join(stack)
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
-    
+
+
+
+print('----------------------------')
+sol = Solution()
+s = "nzp#o#g"
+t = "b#nzp#o#g"
+expected = True
+result = sol.backspaceCompare(s,t)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')  
+
+
+print('----------------------------')
+sol = Solution()
+s = "bbbextm"
+t = "bbb#extm"
+expected = False
+result = sol.backspaceCompare(s,t)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')  
+
+
+
+print('----------------------------')
+sol = Solution()
+s = "ab##"
+t = "c#d#"
+expected = True
+result = sol.backspaceCompare(s,t)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')  
+
+print('----------------------------')
+sol = Solution()
+s = "ab#c"
+t = "ad#c"
+expected = True
+result = sol.backspaceCompare(s,t)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')    
+
+print('----------------------------')
 sol = Solution()
 s = "a#c"
 t = "b"
