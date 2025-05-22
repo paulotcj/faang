193e2b64371e934b235ee754d805fe527eb1593b
Paulo <Costa>
2025-05-22 00:15:22 -0400

    .

--- python/leetcode/042_Arrays_trapping_rain_water/4_solution.py
+++ python/leetcode/042_Arrays_trapping_rain_water/4_solution.py
@@ -9,26 +9,61 @@ class Solution:
         
         if len_hei == 0: return 0
 
-        left  : int = 0
-        right : int = len_hei - 1
-        left_max  : int = 0
-        right_max : int = 0
+        left_idx  : int = 0
+        right_idx : int = len_hei - 1
+        left_max_v  : int = 0
+        right_max_v : int = 0
         total_trapped_water : int = 0
 
         #-----------------------------------
-        while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max : int = height[left]
+        while left_idx < right_idx:
+            left_v : int = height[left_idx]
+            right_v : int = height[right_idx]
+            
+            #----------
+            if left_v < right_v:
+                if left_v >= left_max_v:
+                    left_max_v : int = left_v
                 else:
-                    total_trapped_water += left_max - height[left]
-                left += 1
+                    total_trapped_water += left_max_v - left_v
+                left_idx += 1
             else:
-                if height[right] >= right_max:
-                    right_max : int = height[right]
+                if right_v >= right_max_v:
+                    right_max_v : int = right_v
                 else:
-                    total_trapped_water += right_max - height[right]
-                right -= 1
+                    total_trapped_water += right_max_v - right_v
+                right_idx -= 1
+            #----------
+        #-----------------------------------
+
+        return total_trapped_water
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def trap2(self, height: List[int]) -> int:
+        len_hei: int = len(height)
+        
+        if len_hei == 0: return 0
+
+        left_idx  : int = 0
+        right_idx : int = len_hei - 1
+        left_max_v  : int = 0
+        right_max_v : int = 0
+        total_trapped_water : int = 0
+
+        #-----------------------------------
+        while left_idx < right_idx:
+            if height[left_idx] < height[right_idx]:
+                if height[left_idx] >= left_max_v:
+                    left_max_v : int = height[left_idx]
+                else:
+                    total_trapped_water += left_max_v - height[left_idx]
+                left_idx += 1
+            else:
+                if height[right_idx] >= right_max_v:
+                    right_max_v : int = height[right_idx]
+                else:
+                    total_trapped_water += right_max_v - height[right_idx]
+                right_idx -= 1
         #-----------------------------------
 
         return total_trapped_water
@@ -40,6 +75,7 @@ sol = Solution()
 input = [0,1,2,1]
 expected = 0
 result = sol.trap(input)
+# print(input)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 print('------------------')
@@ -49,6 +85,7 @@ sol = Solution()
 input = [0,1,0,2,1,0,1,3,2,1,2,1]
 expected = 6
 result = sol.trap(input)
+# print(input)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 print('------------------')
@@ -57,6 +94,7 @@ sol = Solution()
 input = [4,2,0,3,2,5]
 expected = 9
 result = sol.trap(input)
+# print(input)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 print('------------------')
@@ -65,15 +103,38 @@ sol = Solution()
 input = [5,5,1,7,1,1,5,2,7,6]
 expected = 23
 result = sol.trap(input)
+# print(input)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 print('------------------')
+
+
 
 sol = Solution()
 input = [9,2,1,1,6,4,0,4,4]
 expected = 18
 result = sol.trap(input)
+# print(input)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 print('------------------')
 
+
+sol = Solution()
+input = [9,2,1,1,6,4,0,4,4,0,0,0]
+expected = 18
+result = sol.trap(input)
+# print(input)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
+print('------------------')
+
+
+sol = Solution()
+input = [0,0,0]
+expected = 0
+result = sol.trap(input)
+# print(input)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
+print('------------------')
