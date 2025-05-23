0bea28c67f6cbdbc8ae5c8085e11aef31546533f
Paulo <Costa>
2025-05-23 17:57:09 -0400

    .

--- python/leetcode/003_strings_longest_substring_without_repeating_characters/1_longest_substring_without_repeating_characters_brute_force.py
+++ python/leetcode/003_strings_longest_substring_without_repeating_characters/1_longest_substring_without_repeating_characters_brute_force.py
@@ -1,31 +1,38 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
+from typing import Dict
+#-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
-        string_dict = {}
-
+    def lengthOfLongestSubstring(self, s : str) -> int:
+        max_len : int = 0
+        substr_dict : Dict[str, int] = {}
+        
+        idx : int = 0            
         #-----------------------------------
-        i = 0
-        while i < len(s):
-            v = s[i]
-
-            if v in string_dict:
-                max_len = max(max_len, len(string_dict))
-
-                i = string_dict[v] + 1 # move i to the next index of the repeated character
-                v = s[i]
-               
-                string_dict = {}
-            else:
-                string_dict[v] = i
-                i += 1
-        #-----------------------------------
+        while idx < len(s):
+            char : str = s[idx]
+            #--------------
+            if char in substr_dict: # substring broken - not unique chars in the substring
+                max_len : int = max(max_len, len(substr_dict))
+                idx : int = substr_dict[char] + 1 # idx needs to assume the next position because otherwise we know it will get stuck in a loop
                 
-        return max(max_len, len(string_dict))
+                substr_dict : Dict[str,int] = {} # reset dict. do not add anything here
+            else: # so far only unique chars
+                substr_dict[char] = idx
+                idx += 1
+            #--------------
+        #-----------------------------------
+        
+        # at this point we need to return the value of the max substring, but it might
+        #   be the case where we didn't accumulate the values substr_dict, so we dot it now
+        return_val : int = max(max_len, len(substr_dict))
+        return return_val
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
+
+
+
     
 print('----------------------------')
 sol = Solution()
@@ -53,8 +60,3 @@ result = sol.lengthOfLongestSubstring(s)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
 
-
-
-
-        
-        
