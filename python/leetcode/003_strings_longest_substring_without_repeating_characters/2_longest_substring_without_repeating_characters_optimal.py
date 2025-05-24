cd07c421109ea0e235002082972da7e84fe2d333
Paulo <Costa>
2025-05-24 01:08:57 -0400

    .

--- python/leetcode/003_strings_longest_substring_without_repeating_characters/2_longest_substring_without_repeating_characters_optimal.py
+++ python/leetcode/003_strings_longest_substring_without_repeating_characters/2_longest_substring_without_repeating_characters_optimal.py
@@ -7,37 +7,34 @@ class Solution:
     #-------------------------------------------------------------------------
     def lengthOfLongestSubstring(self, s : str) -> int :
         input_str : str = s
-        str_dict : Dict[str, int] = {}
-        max_len : int = 0
+        str_dict  : Dict[str, int] = {}
+        max_len   : int = 0
+        curr_len  : int = 0
         begin_substr_idx : int = 0
         
         #-----------------------------------
         for curr_idx , curr_char in enumerate(input_str):
 
+            #--------------
             # breaking the substring condition - have we seen this char? and if so, is its index after
             #   our substring started?
-            #--------------
             if curr_char in str_dict and str_dict[curr_char] >= begin_substr_idx:
                 
-                begin_substr_idx : int = max( begin_substr_idx, str_dict[curr_char] + 1 )
+                max_len : int = max(max_len, curr_len) # before starting a new substring let's keep track of how long this one was
                 
-                if begin_substr_idx != curr_idx:
-                    print('debug')
+                # this get the index of the previous time we saw the repeated char and then skips 1 char ahead                
+                begin_substr_idx : int = str_dict[curr_char] + 1
                 
-                # begin_substr_idx : int = curr_idx
-                str_dict[curr_char] = curr_idx
+                str_dict[curr_char] = curr_idx # keep track where we last seen this char
                 
-            
             else: # first time seein this char
-                str_dict[curr_char] = curr_idx
+                str_dict[curr_char] = curr_idx # keep track where we last seen this char
                 
-                # if we have a subsstring that is from low_idx = 2 and higher_idx = 4, we want to
-                #   include indexes: 2, 3, 4. But using the subtraction we would have: 4 - 2 = 2
-                #   therefore we need to add + 1
                 curr_len : int = curr_idx - begin_substr_idx + 1
-                max_len : int = max(max_len, curr_len)
         #--------------            
         #-----------------------------------
+        
+        max_len : int = max(max_len, curr_len) # we might need to update max_len here, as the last (few) loops might all be new unique chars only hitting the else condition from the loop
         
         return max_len
     #-------------------------------------------------------------------------
@@ -50,7 +47,7 @@ expected = 3
 result = sol.lengthOfLongestSubstring(s)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')    
-exit() 
+
     
     
 print('----------------------------')
