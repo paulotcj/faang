21c5b5851f272111558a15a0c7274e5d9fa4bdcb
Paulo <Costa>
2025-05-25 06:56:20 -0400

    .

--- python/leetcode/125_Valid_Palindrome_and_almost_palindrome/3_valid_palindrome_optimal.py
+++ python/leetcode/125_Valid_Palindrome_and_almost_palindrome/3_valid_palindrome_optimal.py
@@ -2,31 +2,29 @@
 
 #-------------------------------------------------------------------------
 class Solution:
-
     #-------------------------------------------------------------------------
-    def isPalindrome(self, s: str) -> bool:
-        s : str = s.lower()
-
-        low_idx: int = 0
-        high_idx: int = len(s) - 1
-        while(low_idx < high_idx):
-
-            while(low_idx < high_idx and not s[low_idx].isalnum() ):
-                low_idx += 1
+    def isPalindrome( self, s : str ) -> bool :
+        low_idx : int = 0
+        high_idx : int = len(s) - 1
         
-            while(low_idx < high_idx and not s[high_idx].isalnum()):
+        #-----------------------------------
+        while low_idx < high_idx :
+            #---------------
+            while low_idx < high_idx and s[low_idx].isalnum() == False :
+                low_idx += 1
+            #---------------
+            while low_idx < high_idx and s[high_idx].isalnum() == False :
                 high_idx -= 1
-
-            if(s[low_idx] != s[high_idx]):
-                return False
+            #---------------
             
-            low_idx+= 1
-            high_idx-= 1
+            if s[low_idx].lower() != s[high_idx].lower() : return False
+            
+            low_idx += 1
+            high_idx -= 1
+        #-----------------------------------
         
         return True
     #-------------------------------------------------------------------------
-
-
 #-------------------------------------------------------------------------
     
 
@@ -39,5 +37,27 @@ expected = True
 result = sol.isPalindrome(s)
 print(f'result: {result}')
 print(f'Is the result correct? { result == expected}')
+
+
+
+print('----------------------------')
+sol = Solution()
+s = "race a car"
+expected = False
+result = sol.isPalindrome(s)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
+
+
+print('----------------------------')
+sol = Solution()
+s = " "
+expected = True
+result = sol.isPalindrome(s)
+print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
+
+
+
 
 
