c7caa583121bf370f039200e76e152d2b8a7d701
Paulo <Costa>
2025-05-27 18:36:54 -0400

    .

--- python/leetcode/092_Linked_List_reverse_linked_list_2/2_reverse_linked_list_2.py
+++ python/leetcode/092_Linked_List_reverse_linked_list_2/2_reverse_linked_list_2.py
@@ -3,40 +3,49 @@ from typing import Optional, List, Tuple
 #-------------------------------------------------------------------------
 class ListNode:
     #-------------------------------------------------------------------------
-    def __init__(self, val=0, next=None):
+    def __init__( self, val : int = 0, next : 'ListNode' = None ):
         self.val = val
-        self.next = next
+        self.next : 'ListNode' = next
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def create_linked_list(self, arr : List[int]) -> ListNode:
-        head : ListNode = None
-        curr : ListNode = None
-
-        for i , v in enumerate(arr):
-            if i == 0:
-                head = ListNode(v)
-                curr = head
+    def create_linked_list(self, arr : List[int] ) -> Optional[ListNode] : 
+        head : Optional[ListNode] = None
+        curr : Optional[ListNode] = None
+        prev : Optional[ListNode] = None
+        
+        #-----------------------------------
+        for idx, val in enumerate(arr):
+            curr = ListNode(val = val)
+            if idx == 0 : # setting up the root node - there's no prev
+                head  = curr
+                prev = curr
             else:
-                curr.next = ListNode(v)
-                curr = curr.next
-        return head
+            
+            # curr is the current node, this is the newest node and we don't point next
+            #   to anything, but we should keep track of the previous node, so it's next
+            #   pointer can point to curr
+                prev.next = curr
+                prev = curr
+        #-----------------------------------
+        return head      
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def print_linked_list(self, head: ListNode) -> List[int]:
-        curr : ListNode = head
-        list : List[int] = []
+    def print_linked_list( self , head : Optional[ListNode] ) -> List[int] :
+        curr : Optional[ListNode] = head
+        return_list : List[int] = []
+        #-----------------------------------
         while curr != None:
-            list.append(curr.val)
+            return_list.append(curr.val)
             curr = curr.next
-
-        print(list)
-        return list 
+        #-----------------------------------
+            
+        return return_list
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def get_in_position(self, head: ListNode, left: int) -> (ListNode, ListNode):
+    def get_in_position(self, head: ListNode, left: int) -> Tuple[ListNode, ListNode]:
         #-----------
         #Get in position - curr is always one node to the right of the left_end, even if it's None
         curr : ListNode = head
@@ -50,7 +59,7 @@ class Solution:
         return curr, left_end        
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def revert(self, left: int, right: int, curr: ListNode)-> (ListNode, ListNode):
+    def revert(self, left: int, right: int, curr: ListNode)-> Tuple[ListNode, ListNode]:
         prev : ListNode = curr
         curr = curr.next
         temp : ListNode = None
@@ -83,20 +92,20 @@ class Solution:
 #-------------------------------------------------------------------------
 
 
-# print('----------------------------')
-# sol = Solution()
-# arr = [3,5]
-# left = 1
-# right = 2
-# expected = [5,3]  
+print('----------------------------')
+sol = Solution()
+arr = [3,5]
+left = 1
+right = 2
+expected = [5,3]  
 
 
-# head = sol.create_linked_list(arr)
-# _ = sol.print_linked_list(head)
-# result = sol.reverseBetween(head, left, right)
-# result = sol.print_linked_list(result)
-# # print(f'result: {result}')
-# print(f'Is the result correct? { result == expected}')
+head = sol.create_linked_list(arr)
+_ = sol.print_linked_list(head)
+result = sol.reverseBetween(head, left, right)
+result = sol.print_linked_list(result)
+# print(f'result: {result}')
+print(f'Is the result correct? { result == expected}')
 # exit()
 
 print('----------------------------')
