01202dce719d8c4ee189aad81db9ff346079d8bf
Paulo <paulotcj@gmail.com>
2025-05-30 02:47:24 -0400

    .

--- python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
+++ python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
@@ -1,4 +1,4 @@
-#problem: https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
 #-------------------------------------------------------------------------
 from typing import Optional, List
 class Node:
@@ -8,21 +8,22 @@ class Node:
         self.prev = prev
         self.next = next
         self.child = child
-        #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 class ProcessList:
     #-------------------------------------------------------------------------
     def CreateFromArray(arr: List[int]):
-        nodes_list : List(Node) = []
-        #---------------
+        nodes_list : List[Node] = []
+        #-----------------------------------
         for i,v in enumerate(arr):
             if v :
                 temp : Node = Node(v, None, None, None)
                 nodes_list.append(temp)
             else:
                 nodes_list.append(None)
-        #---------------
+        #-----------------------------------
+        
         prev : Node = None
         curr : Node = None
         next : Node = None
@@ -39,26 +40,28 @@ class ProcessList:
                 prev.next = curr
 
             prev = curr
-        #---------------
+        #-----------------------------------
         idx_header, step_count, i = 0, 0, 0
         while i < len(nodes_list):
+            
+            #-----------------------------------
             if nodes_list[i] is None:
                 step_count = -1
 
+                #-----------------------------------
                 while nodes_list[i] is None and i < len(nodes_list):
                     step_count += 1
                     i += 1
+                #-----------------------------------
 
                 if nodes_list[i] is not None and i < len(nodes_list):
                     nodes_list[idx_header + step_count].child = nodes_list[i]
                     idx_header = i
+            #-----------------------------------
 
-            #------
+            
             i += 1
-
-        
-
-        #---------------
+        #-----------------------------------
             
         return nodes_list[0] if len(nodes_list) > 0 else None
     #-------------------------------------------------------------------------
@@ -66,9 +69,12 @@ class ProcessList:
     def print_linked_list (head: Node) -> List[int]:
         curr : Node = head
         list : List[int] = []
+        
+        #-----------------------------------
         while curr != None:
             list.append(curr.val)
             curr = curr.next
+        #-----------------------------------
 
         print(list)
         return list 
@@ -82,31 +88,35 @@ class Solution:
         list_nodes : List[Node] = []
         stack_pending_nodes : List[Node] = []
 
-        #-------------
+        #-----------------------------------
         while curr:
 
             list_nodes.append(curr)
 
             if curr.child: #if the node has a child start processing the child's list
-                stack_pending_nodes.append(curr.next)
-                #---
-                curr.child.prev = curr
-                curr.next = curr.child
-                curr.child = None
-                #---
-                # curr = curr.next
-                # continue
+                stack_pending_nodes.append(curr)
+                curr = curr.child
+                continue
 
             curr = curr.next
+            #-----------------------------------
             while curr is None and len(stack_pending_nodes) > 0:
-                curr = stack_pending_nodes.pop()
-                list_nodes[-1].next = curr
-                if curr:
-                    curr.prev = list_nodes[-1]
-
-        #-------------
-
+                curr = stack_pending_nodes.pop().next
+            #-----------------------------------
+        #-----------------------------------
                 
+        #-----------------------------------
+        for i, v in enumerate(list_nodes):
+            #curr - no need to change
+            #prev
+            if i > 0:
+                list_nodes[i].prev = list_nodes[i-1]
+            #next
+            if i < (len(list_nodes) - 1): #must be less: eg: len = 10, len -1 = 9, so i must be less or equal to 8
+                list_nodes[i].next = list_nodes[i+1]
+            #child
+            list_nodes[i].child = None
+        #-----------------------------------
 
         return head
     #-------------------------------------------------------------------------
@@ -147,4 +157,3 @@ print(f'Is the result correct? { result == expected}')
 # exit()
 
 
-print('hi')
--- python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/2_flatten_multilevel_doubly_linked_list.py
+++ python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/2_flatten_multilevel_doubly_linked_list.py
@@ -1,4 +1,4 @@
-#problem: https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
 #-------------------------------------------------------------------------
 from typing import Optional, List
 class Node:
@@ -88,27 +88,25 @@ class Solution:
             list_nodes.append(curr)
 
             if curr.child: #if the node has a child start processing the child's list
-                stack_pending_nodes.append(curr)
-                curr = curr.child
-                continue
+                stack_pending_nodes.append(curr.next)
+                #---
+                curr.child.prev = curr
+                curr.next = curr.child
+                curr.child = None
+                #---
+                # curr = curr.next
+                # continue
 
             curr = curr.next
             while curr is None and len(stack_pending_nodes) > 0:
-                curr = stack_pending_nodes.pop().next
+                curr = stack_pending_nodes.pop()
+                list_nodes[-1].next = curr
+                if curr:
+                    curr.prev = list_nodes[-1]
+
         #-------------
+
                 
-        #-------------
-        for i, v in enumerate(list_nodes):
-            #curr - no need to change
-            #prev
-            if i > 0:
-                list_nodes[i].prev = list_nodes[i-1]
-            #next
-            if i < (len(list_nodes) - 1): #must be less: eg: len = 10, len -1 = 9, so i must be less or equal to 8
-                list_nodes[i].next = list_nodes[i+1]
-            #child
-            list_nodes[i].child = None
-        #-------------
 
         return head
     #-------------------------------------------------------------------------
