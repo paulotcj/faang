661b621a1b161a929315e12165e971a49efb8364
Paulo <paulotcj@gmail.com>
2025-06-03 18:21:43 -0400

    .

--- python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
+++ python/leetcode/430_Linked_List_flatten_multilevel_doubly_linked_list/1_flatten_multilevel_doubly_linked_list.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list
-#-------------------------------------------------------------------------
+
+
 from typing import Optional, List
+#-------------------------------------------------------------------------
 class Node:
     #-------------------------------------------------------------------------
     def __init__(self, val, prev, next, child):
@@ -13,12 +15,14 @@ class Node:
 #-------------------------------------------------------------------------
 class ProcessList:
     #-------------------------------------------------------------------------
-    def CreateFromArray(arr: List[int]):
+    def create_from_array( arr : List[int] ) -> Node :
         nodes_list : List[Node] = []
+        
+        # create the list
         #-----------------------------------
-        for i,v in enumerate(arr):
-            if v :
-                temp : Node = Node(v, None, None, None)
+        for loop_idx, loop_val in enumerate(arr) :
+            if loop_val :
+                temp : Node = Node(val = loop_val, prev = None , next = None, child = None)
                 nodes_list.append(temp)
             else:
                 nodes_list.append(None)
@@ -27,57 +31,70 @@ class ProcessList:
         prev : Node = None
         curr : Node = None
         next : Node = None
-        for i,v in enumerate(nodes_list):
-            if v is None:
+        
+        #adjust next and prev pointers
+        #-----------------------------------
+        for loop_idx, loop_val in enumerate(nodes_list) :
+            # this means this level has ended and we are about to start a new sublevel list, 
+            #  so we reset all pointers
+            #--------
+            if loop_val is None : 
                 prev = None
                 curr = None
                 next = None
                 continue
-                
-            curr = v
-            curr.prev = prev
-            if prev is not None:
-                prev.next = curr
-
-            prev = curr
-        #-----------------------------------
-        idx_header, step_count, i = 0, 0, 0
-        while i < len(nodes_list):
+            #--------
             
+            curr = loop_val
+            curr.prev = prev
+            if prev is not None: prev.next = curr
+            
+            prev = curr # adjust the pointer before the next loop
+        #-----------------------------------
+        
+        parent_list_start, i = 0, 0
+        #-----------------------------------
+        while i < len(nodes_list):
             #-----------------------------------
-            if nodes_list[i] is None:
-                step_count = -1
-
+            if nodes_list[i] is None : # this first none indicates a new sublist has started
+                sublist_offset : int = -1 # assumes -1 since the first None will count +1 and it will be position 0
+                
+                # position the sublist_offset
                 #-----------------------------------
                 while nodes_list[i] is None and i < len(nodes_list):
-                    step_count += 1
+                    sublist_offset += 1
                     i += 1
                 #-----------------------------------
-
-                if nodes_list[i] is not None and i < len(nodes_list):
-                    nodes_list[idx_header + step_count].child = nodes_list[i]
-                    idx_header = i
+                
+                # now parent_list_start is pointing at the index where this list starts
+                #  sublist_offset tells how many steps we need to skip to find the parent
+                #  node of this new sublist
+                
+                nodes_list[ parent_list_start + sublist_offset ].child = nodes_list[i]
+                
+                # now this is a potential start for a new sublist, so we keep track of it
+                parent_list_start = i                
             #-----------------------------------
-
-            
-            i += 1
+            i += 1 # loop
         #-----------------------------------
-            
-        return nodes_list[0] if len(nodes_list) > 0 else None
+        
+        # now what to return? nodes_list[0]
+        return_obj : Node = nodes_list[0] if nodes_list else None
+        return return_obj
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
-    def print_linked_list (head: Node) -> List[int]:
+    def print_linked_list(head : Node) -> List[int] :
         curr : Node = head
         list : List[int] = []
-        
         #-----------------------------------
-        while curr != None:
+        while curr is not None:
             list.append(curr.val)
             curr = curr.next
         #-----------------------------------
-
+        
         print(list)
-        return list 
+        return list
+        
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
@@ -131,7 +148,7 @@ arr = [1,2,3,4,5,6,None,None,None,7,8,None,None,11,12]
 #       11,12
 expected = [1,2,3,7,8,11,12,4,5,6]
 # error generated: [1,2,3,7,8,11,12]
-head = ProcessList.CreateFromArray(arr)
+head = ProcessList.create_from_array(arr = arr)
 
 
 head = sol.flatten(head)
@@ -139,14 +156,14 @@ result = ProcessList.print_linked_list(head)
 print(f'result  : {result}')
 print(f'expected: {expected}')
 print(f'Is the result correct? { result == expected}')
-exit()
+
 
 print('----------------------------')
 sol = Solution()
 arr = [ 1, 2, 3, 4, 5, 6, None, None, None, 7, 8, 9, 10, None, None, 11, 12 ]
 
 expected = [1,2,3,7,8,11,12,9,10,4,5,6]
-head = ProcessList.CreateFromArray(arr)
+head = ProcessList.create_from_array(arr = arr)
 
 
 head = sol.flatten(head)
