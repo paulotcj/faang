9325acec405f58021f0b07dc9e10c6081cb523a2
Paulo <paulotcj@gmail.com>
2025-06-05 18:07:09 -0400

    .

--- python/leetcode/1249_Stacks_Minimum_Brackets_to_Remove_to_Make_Valid_Parentheses/1_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ python/leetcode/1249_Stacks_Minimum_Brackets_to_Remove_to_Make_Valid_Parentheses/1_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -26,47 +26,13 @@ class Solution:
         
         scheduled_for_removal.extend(stack_opening) # merge them for easier manipulation
         
-        # #-----------------------------------
-        # while scheduled_for_removal:
-        #     remove_idx : int = scheduled_for_removal.pop()
-        #     s = s[0:remove_idx] + s[remove_idx+1:] #this is: get the string from 0 to remove_idx (being remove_idx not included) and then from remove_idx+1 until the end
-        # #-----------------------------------
-        char_list : list[str] = list(s)
         #-----------------------------------
         while scheduled_for_removal:
             remove_idx : int = scheduled_for_removal.pop()
-            char_list[remove_idx] = None
-        #-----------------------------------        
-        
+            s = s[0:remove_idx] + s[remove_idx+1:] #this is: get the string from 0 to remove_idx (being remove_idx not included) and then from remove_idx+1 until the end
+        #-----------------------------------
+     
         # string is now clean
-        result : str = ''.join( c for c in char_list if c is not None )
-        print(result)
-        return result
-    #-------------------------------------------------------------------------
-    #-------------------------------------------------------------------------
-    def minRemoveToMakeValid2(self, s: str) -> str:
-        stack_opening = []
-        scheduled_for_removal = []
-        #-----------------------------------
-        for i, v in enumerate(s):
-            #-----
-            if v == '(': #we don't know anything yet
-                stack_opening.append(i)
-            elif v == ')': #when closing we need to check whether the stack is empty and if not try to close it
-                #-----
-                if stack_opening: stack_opening.pop()
-                else: scheduled_for_removal.append(i)
-                #-----
-            #-----
-        #-----------------------------------
-
-        #we need to remove the remaining elements in the stack
-        scheduled_for_removal.extend(stack_opening)
-        #-----------------------------------
-        while scheduled_for_removal:
-            i = scheduled_for_removal.pop()
-            s = s[:i] + s[i+1:] #this is: get the string from 0 to i (being i not included) and then from i+1 until the end
-        #----------------------------------- 
         return s
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
