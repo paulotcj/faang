8ad372e89746594c2d2b8d753be8017ae6390f78
Paulo <paulotcj@gmail.com>
2025-06-05 03:34:00 -0400

    .

--- python/leetcode/020_Stacks_valid_parentheses/2_valid_parentheses.py
+++ python/leetcode/020_Stacks_valid_parentheses/2_valid_parentheses.py
@@ -1,10 +1,41 @@
 #problem: https://leetcode.com/problems/valid-parentheses/description/
 
 from typing import List, Dict
+
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def isValid(self, s: str) -> bool:
+    def isValid( self , s : str ) -> bool :
+        char_stack : list[str] = []
+        dict_opening : dict[str,str] = { '(':')' , '{':'}' , '[':']' }
+        dict_closing : dict[str,str] = { ')':'(' , '}':'{' , ']':'[' }
+        
+        #-----------------------------------
+        for c in s :
+            #-----
+            if c in dict_opening.keys() : 
+                char_stack.append(c)
+                continue
+            elif not char_stack : return False # not an opening char, and if the stack is empty, return False
+            #-----
+            #-----
+            # c is a closing char, then whatever is in the stack in order to be valid must be
+            #   its opening counter part. if so, continue to process, if not return false
+            check_if_valid_opening : str = char_stack[-1] 
+            potential_closing_char : str = c
+            if check_if_valid_opening == dict_closing[potential_closing_char] :
+                char_stack.pop() # matching opening and closing chars. pop stack and continue to process
+            else:
+                return False # chars didn't match
+            #-----
+        #-----------------------------------
+        # until now no mismatch was found, however it's possible that the stack still has
+        #  chars in it. If so, the string did not matched all its parenthesis or brackets        
+        return_result : bool = False if char_stack else True
+        return return_result
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def isValid2(self, s: str) -> bool:
         char_stack : List[str] = []
         dict_opening : Dict[str:str] = { '(': ')', '{': '}', '[': ']'  }
         dict_closing : Dict[str:str] = { ')': '(', '}': '{', ']': '['  }
@@ -15,7 +46,7 @@ class Solution:
                 char_stack.append(c)
                 continue
             
-            if not char_stack: #closing bracket without an opening bracket
+            elif not char_stack: #closing bracket without an opening bracket
                 return False
                 
             temp:str = char_stack[-1] #potentially (, {, or [
