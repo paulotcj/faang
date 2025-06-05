1b9fd2fc4b2c2a40008861706ec4e6bfef014bf2
Paulo <paulotcj@gmail.com>
2025-06-05 05:15:45 -0400

    .

--- python/leetcode/020_Stacks_valid_parentheses/3_valid_parentheses.py
+++ python/leetcode/020_Stacks_valid_parentheses/3_valid_parentheses.py
@@ -4,6 +4,28 @@ from typing import List, Dict
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
+    def isValid2( self , s : str ) -> bool :
+        stack : list[str] = []
+        bracket_map : dict[str,str] = { ')':'(' , '}':'{' , ']':'[' }
+        
+        for c in s:
+            #-----
+            if c in bracket_map.values(): # it's an opening bracket char, push to the stack
+                stack.append(c)
+            elif c in bracket_map : # it's a closing bracket char, check for matching opening
+                #-----
+                if not stack or stack[-1] != bracket_map[c] : # stack is empty or the char does not match
+                    return False
+                
+                stack.pop()
+                #-----
+            #-----
+            
+            
+                    
+        
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def isValid(self, s: str) -> bool:
         # Stack to keep track of opening brackets
         stack : list[str] = []
