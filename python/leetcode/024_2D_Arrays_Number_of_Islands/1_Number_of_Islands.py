11f4488439cbee83238b910b1078d1e3400f24a8
Paulo <paulotcj@gmail.com>
2025-09-01 00:49:18 -0400

    .

--- python/leetcode/024_2D_Arrays_Number_of_Islands/1_Number_of_Islands.py
+++ python/leetcode/024_2D_Arrays_Number_of_Islands/1_Number_of_Islands.py
@@ -1,62 +1,62 @@
 #problem: https://leetcode.com/problems/number-of-islands
-from typing import List, Dict
+
+
+from collections import deque
+
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-
-        self.num_islands : int = 0
-        self.seen : List[List[bool]] = []
-        self.directions : List[List[int]] = self.get_directions()
-        self.seen : List[List[bool]] = [ [False for col in row] for row in grid]
-        self.queue : List[List[int]] = []
-
-        #----------------
-        for r_idx, row in enumerate(grid):
-            for c_idx, col in enumerate(row):
-                #if this cell is 1 and we never seen it before so it's a new island
-                if grid[r_idx][c_idx] == '1' and not self.seen[r_idx][c_idx]:
-                    self.num_islands += 1
-                    self.seen[r_idx][c_idx] = True
-                    
-                    self.explore_island(r_idx, c_idx, grid)
-        #----------------
-        return self.num_islands
+    def _is_valid(self, row: int, col: int) -> bool: # review pending
+        if row < 0 or row >= len(self.grid): return False
+        if col < 0 or col >= len(self.grid[0]): return False
+        return True
+    #-------------------------------------------------------------------------    
     #-------------------------------------------------------------------------
-    #-------------------------------------------------------------------------
-    def explore_island(self, row: int, col: int, grid: List[List[int]]) -> None:
-        self.enqueue_directions(row, col, grid)
-        while self.queue:
-            row, col = self.queue.pop(0)
-            if grid[row][col] == '1' and self.seen[row][col] == False:
-                self.seen[row][col] = True
-                self.enqueue_directions(row, col, grid)
-    #-------------------------------------------------------------------------
-    #-------------------------------------------------------------------------
-    def enqueue_directions(self, row: int, col: int, grid: List[List[int]]) -> None:
+    def _enqueue_directions(self, row: int, col: int) -> None: # review pending
 
         for dir in self.directions:
             new_row = row + dir[0]
             new_col = col + dir[1]
-            if self.is_valid(new_row, new_col, grid):
+            if self._is_valid(new_row, new_col):
                 self.queue.append([new_row, new_col])
+    #-------------------------------------------------------------------------    
     #-------------------------------------------------------------------------
+    def _explore_island(self, row: int, col: int) -> None: # review pending
+        self._enqueue_directions(row, col)
+        while self.queue:
+            row, col = self.queue.popleft()
+            if self.grid[row][col] == '1' and self.seen[row][col] == False:
+                self.seen[row][col] = True
+                self._enqueue_directions(row, col)
+    #------------------------------------------------------------------------- 
     #-------------------------------------------------------------------------
-    def is_valid(self, row: int, col: int, grid: List[List[int]]) -> bool:
-        if row < 0 or row >= len(grid): return False
-        if col < 0 or col >= len(grid[0]): return False
-        return True
-    #-------------------------------------------------------------------------
-    #-------------------------------------------------------------------------
-    def get_directions(self) -> List[List[int]]:
-        directions : List[List[int]] = [
-            [-1, 0], # up
-            [0, 1], # right  
-            [1, 0], # down
-            [0, -1] # left
+    def numIslands(self , grid : list[list[str]]) -> int : # working
+        # if not grid or grid[0] : return 0
+        rows : int = len(grid)
+        cols : int = len(grid[0])
+        self.num_islands : int = 0
+        self.grid = grid
+        self.seen : list[list[bool]] = [ 
+            [False] * cols 
+            for _ in range(rows)
         ]
-        return directions
+        self.directions : list[list[int]] = [ [-1,0] , [0,1] , [1,0] , [0,-1] ] # up , right , down , left
+        self.queue : deque[list[int]] = deque()
+
+        #----------------------------------------
+        for for_r in range(rows) :
+            for for_c in range(cols) :
+                if self.seen[for_r][for_c] == True : continue
+                if grid[for_r][for_c] == '1' :
+                    self.seen[for_r][for_c] = True
+                    self.num_islands += 1
+
+                    self._explore_island(row = for_r , col = for_c)
+        #----------------------------------------
+
+        return self.num_islands
+        
+    #-------------------------------------------------------------------------   
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
@@ -98,3 +98,22 @@ result = x.numIslands(grid)
 print(f'expected: {expected}')
 print(f'result: {result}')
 print(f'pass: {expected == result}')
+print('----------')
+
+x = Solution()
+grid, expected = aux.create_grid_1()
+
+result = x.numIslands(grid)
+print(f'expected: {expected}')
+print(f'result: {result}')
+print(f'pass: {expected == result}')
+print('----------')
+
+x = Solution()
+grid, expected = aux.create_grid_2()
+
+result = x.numIslands(grid)
+print(f'expected: {expected}')
+print(f'result: {result}')
+print(f'pass: {expected == result}')
+print('----------')
