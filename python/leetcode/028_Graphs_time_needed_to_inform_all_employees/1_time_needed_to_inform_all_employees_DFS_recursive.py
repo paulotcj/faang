10c5d0cbf207adfc368a37da60a18d5444670f93
Paulo <paulotcj@gmail.com>
2025-09-17 19:03:35 -0400

    .

--- python/leetcode/028_Graphs_time_needed_to_inform_all_employees/1_time_needed_to_inform_all_employees_DFS_recursive.py
+++ python/leetcode/028_Graphs_time_needed_to_inform_all_employees/1_time_needed_to_inform_all_employees_DFS_recursive.py
@@ -1,45 +1,49 @@
 # https://leetcode.com/problems/time-needed-to-inform-all-employees/description/
 
-
 #-------------------------------------------------------------------------
-class Solution:
+class Solution : 
     #-------------------------------------------------------------------------
-    def numOfMinutes(self, n, head_id, managers, inform_time):
-        subordinates = [[] for _ in range(n)] # creates the adjacency list
-        
-        #---------------------------
-        for employee in range(n):
-            manager = managers[employee] # who is the manager of that employee
-            
-            if manager == -1: # no manager, root node
-                continue
-            
-            subordinates[manager].append(employee) # adjacency list, adds employees IDs under the manager
-        #---------------------------
-        
-        return self.dfs(head_id, subordinates, inform_time)
+    def dfs( self, current_id : int ) -> int :
+        if not self.subordinates[current_id] : return 0
+
+        max_time : int = 0
+        subordinates : list[int] = self.subordinates[current_id]
+        #----------------------------------------
+        for subordinate_id in subordinates:
+            max_time = max( max_time , self.dfs( current_id=subordinate_id ) )
+        #----------------------------------------
+
+        return_val = int = max_time + self.inform_time [current_id]
+
+        return return_val    
     #-------------------------------------------------------------------------
-    #-------------------------------------------------------------------------
-    def dfs(self, current_id, adj_list, inform_time):
-        if not adj_list[current_id]:
-            return 0
-        
-        max_time = 0
-        subordinates = adj_list[current_id]
-        for subordinate in subordinates:
-            max_time = max(max_time, self.dfs(subordinate, adj_list, inform_time))
-        
-        return max_time + inform_time[current_id]
-    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------  
+    def numOfMinutes(self, n: int, headID: int, manager: list[int], informTime: list[int]) -> int:
+        self.subordinates : list[list[int]] = [
+            [] for _ in range(n)
+        ] # creates the adjacency list
+
+        self.inform_time : list[int] = informTime
+
+        #----------------------------------------
+        for employee_id in range(n) :
+            manager_id : int = manager[employee_id] # who is the manager of that employee
+
+            if manager_id == -1 : continue # no manager, root node
+
+            self.subordinates[manager_id].append(employee_id) # adjacency list, adds employees IDs under the manager
+        #----------------------------------------
+        return_val : int = self.dfs(current_id = headID)
+        return return_val
+    #-------------------------------------------------------------------------  
+          
 #-------------------------------------------------------------------------
 
 managers_array    = [2, 2, 4, 6, -1, 4, 4, 5]
 inform_time_array = [0, 0, 4, 0, 7, 3, 6, 0]
-
+expected : int = 13
 sol = Solution()
+result : int = sol.numOfMinutes(n = 8, headID = 4, manager=managers_array, informTime= inform_time_array)
 
+print(f'Result : {result} - expected : {expected} - is the result the same? {result==expected}')
 
-print(sol.numOfMinutes(8, 4, managers_array, inform_time_array))
-
-
-# 13
