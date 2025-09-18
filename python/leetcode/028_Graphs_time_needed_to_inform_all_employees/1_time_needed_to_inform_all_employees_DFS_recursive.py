e890efafc3700d8f486d529d1702b9bf263538b4
Paulo <paulotcj@gmail.com>
2025-09-17 21:04:03 -0400

    .

--- python/leetcode/028_Graphs_time_needed_to_inform_all_employees/1_time_needed_to_inform_all_employees_DFS_recursive.py
+++ python/leetcode/028_Graphs_time_needed_to_inform_all_employees/1_time_needed_to_inform_all_employees_DFS_recursive.py
@@ -3,40 +3,51 @@
 #-------------------------------------------------------------------------
 class Solution : 
     #-------------------------------------------------------------------------
-    def dfs( self, current_id : int ) -> int :
-        if not self.subordinates[current_id] : return 0
+    def dfs( self, current_emp_id : int ) -> int :
+        curr_subordinates : list[int] = self.subordinates_adj_list[current_emp_id]
+        if not curr_subordinates : return 0 # if no subordinates, there's no time needed to inform - return 0
 
+        # now we look at all subordinates to this employee. If its subordinates don't have anyone below
+        #   them, then they will return time_required = 0. If they do have anyone underneath them then
+        #   they will return the time needed to inform their respective subordinates, going down all the
+        #   way in the graph.
+        # Some subordinates may need more time to inform than other subordinates, their graph might
+        #   be deeper with longer inform times, im that case we need to keep track of max_time
+        #   between all direct subordinates from this current_emp_id
+        # Eventually the algorithm will receive back the time needed to inform all subordinates of this
+        #   current_emp_id, then we need to add the time it takes this current_emp_id to inform the
+        #   subordinates
         max_time : int = 0
-        subordinates : list[int] = self.subordinates[current_id]
         #----------------------------------------
-        for subordinate_id in subordinates:
-            max_time = max( max_time , self.dfs( current_id=subordinate_id ) )
+        for subordinate_id in curr_subordinates:
+            max_time = max( max_time , self.dfs( current_emp_id=subordinate_id ) )
         #----------------------------------------
 
-        return_val = int = max_time + self.inform_time [current_id]
+        return_val : int = max_time + self.inform_time [current_emp_id]
 
         return return_val    
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------  
     def numOfMinutes(self, n: int, headID: int, manager: list[int], informTime: list[int]) -> int:
-        self.subordinates : list[list[int]] = [
+        # step 1 - just create and populate an adjacency list
+        self.subordinates_adj_list : list[list[int]] = [
             [] for _ in range(n)
         ] # creates the adjacency list
 
         self.inform_time : list[int] = informTime
 
         #----------------------------------------
-        for employee_id in range(n) :
+        for employee_id in range(n) : # fill in the adjacency list
             manager_id : int = manager[employee_id] # who is the manager of that employee
 
             if manager_id == -1 : continue # no manager, root node
 
-            self.subordinates[manager_id].append(employee_id) # adjacency list, adds employees IDs under the manager
+            self.subordinates_adj_list[manager_id].append(employee_id) # adjacency list, adds employees IDs under the manager
         #----------------------------------------
-        return_val : int = self.dfs(current_id = headID)
+        # step 2 - explore the adjacency list DFS style - return the max time to inform
+        return_val : int = self.dfs(current_emp_id = headID) # now explore the graph, DFS
         return return_val
     #-------------------------------------------------------------------------  
-          
 #-------------------------------------------------------------------------
 
 managers_array    = [2, 2, 4, 6, -1, 4, 4, 5]
