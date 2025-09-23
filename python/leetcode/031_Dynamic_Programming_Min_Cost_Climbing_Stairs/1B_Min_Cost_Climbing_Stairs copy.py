c9fd9328f1f648fa899b0e6e1e2fb254f2b9ed07
Paulo <paulotcj@gmail.com>
2025-09-22 23:11:13 -0400

    .

--- python/leetcode/031_Dynamic_Programming_Min_Cost_Climbing_Stairs/1B_Min_Cost_Climbing_Stairs copy.py
+++ python/leetcode/031_Dynamic_Programming_Min_Cost_Climbing_Stairs/1B_Min_Cost_Climbing_Stairs copy.py
@@ -1,29 +1,26 @@
 # https://leetcode.com/problems/min-cost-climbing-stairs/description/
 
 #-------------------------------------------------------------------------
-class Solution_og : 
+class Solution : 
     #-------------------------------------------------------------------------
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        cost.append(0)
+        total_cost : int = 0
 
-        sum_cost : list[int] = [0] * len(cost)
-
+        cost.append(0) # adding this one just for simplicity and easy to loop the array so we don't need to check limits
+        i : int = 0
         #----------------------------------------
-        for idx , val in enumerate(cost) :
-            #-----
-            if idx == 0 or idx == 1 :
-                sum_cost[idx] = cost[idx]
-                continue
-            #-----
+        while i < len(cost)-1:
+            if  cost[i+1] <= cost[i] : 
+                i += 1
 
-            sum_cost[idx] = cost[idx] + min( sum_cost[idx-1] , sum_cost[idx-2] )
+            total_cost += cost[i] 
+            i += 1
         #----------------------------------------
 
-        result : int = sum_cost[-1]
-
-        return result
+        return total_cost
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
+
 
 sol = Solution()
 cost = [10,15,20]
