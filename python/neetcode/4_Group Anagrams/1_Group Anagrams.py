d038cc0ebc1b0ae013b0fc8162b4ed5b9a219674
Paulo Costa <paulotcj@gmail.com>
2025-12-03 20:00:23 -0500

    .

--- python/neetcode/4_Group Anagrams/1_Group Anagrams.py
+++ python/neetcode/4_Group Anagrams/1_Group Anagrams.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 class Solution:
     #-------------------------------------------------------------------------
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        temp_dict = defaultdict( list ) # create a default dict with a default value of a list
+        temp_dict: defaultdict[str, list[str]] = defaultdict(list)  # create a default dict with a default value of a list
         #--------------------------------------------------
-        for loop_str in strs :
-            temp_sorted_str = ''.join( sorted(loop_str) ) # make a sorted version of the loop string
-            temp_dict[temp_sorted_str].append(loop_str) # make the sorted version the dict key, and append the sorted string to the list
+        for loop_str in strs:
+            temp_sorted_str: str = ''.join(sorted(loop_str))  # make a sorted version of the loop string
+            temp_dict[temp_sorted_str].append(loop_str)  # make the sorted version the dict key, and append the sorted string to the list
         #--------------------------------------------------
 
-        return_obj : list[list[str]] = list( temp_dict ) # convert the dict in a list
-        return  return_obj # now we have all the anagram pairs combined in lists inside a list
+        return_obj: list[list[str]] = list(temp_dict.values())  # convert the dict values to a list of lists
+        return return_obj  # now we have all the anagram pairs combined in lists inside a list
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 
