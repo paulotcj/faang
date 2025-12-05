7b8631f9d65261fc96b7910234e1dddcf1991857
Paulo Costa <paulotcj@gmail.com>
2025-12-04 22:19:57 -0500

    .

--- python/neetcode/5_Top K Frequent Elements/3_Top K Frequent Elements.py
+++ python/neetcode/5_Top K Frequent Elements/3_Top K Frequent Elements.py
@@ -1,21 +1,57 @@
 from collections import defaultdict
-import heapq
+
 #-------------------------------------------------------------------------
 class Solution:
     #-------------------------------------------------------------------------
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        pass
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
+        #--------------------------------------------------
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        #--------------------------------------------------
+        #--------------------------------------------------            
+        for num, cnt in count.items(): # now transfer the dict to a map of frequency per numbers list
+            freq[cnt].append(num)
+        #--------------------------------------------------
+        print(freq)
+        # exit()
+        res = []
+        #--------------------------------------------------
+        for idx in reversed(range(1, len(freq))):
+            print(f'idx:{idx}')
+            for num in freq[idx]: # for all the numbers in this frequency bucket...
+                res.append(num) # add to the response
+                if len(res) == k:
+                    return res
+        #--------------------------------------------------
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------                
+#-------------------------------------------------------------------------
+class Solution:
+    #-------------------------------------------------------------------------
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count : defaultdict = defaultdict(lambda:0)
+        freq : list[int] = [0] * (len(nums) + 1) 
+        #--------------------------------------------------
+        for num in nums : 
+            count[num] += 1
         #--------------------------------------------------
 
         #--------------------------------------------------
-
-
-        #--------------------------------------------------
-
+        for num , cnt in count.items() : # now transfer the dict to a map of frequency per numbers list
+            freq[cnt] = num
         #--------------------------------------------------  
 
+        # loop in reverse order (since we are looking for the highest incidence number count) and add to the response list
+        #  if there's an actual count. Then you slice it getting only the top k numbers with the highest count
+        return_obj : list[int] = [ 
+            num for num in freq[::-1] 
+            if num > 0
+        ][:k]
 
+        return return_obj
     #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
         
