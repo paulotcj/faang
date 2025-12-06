ed1e576b2ac025cab26c2c7326c4504ce0885994
Paulo <paulotcj@gmail.com>
2025-12-05 21:25:30 -0500

    .

---
+++ python/neetcode/6_Encode and Decode Strings/1_Encode and Decode Strings.py
@@ -0,0 +1,66 @@
+#-------------------------------------------------------------------------
+class Solution:
+    #-------------------------------------------------------------------------
+    def encode(self, strs: list[str]) -> str:
+        if not strs : return ""
+
+        sizes : list[str] = [ str(len(loop_str)) for loop_str in strs ]
+
+        return_obj : str = f"{ ','.join(sizes) }#{ ''.join(strs) }"
+
+        return return_obj
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    def decode(self, s: str) -> list[str]:
+        if not s: return []
+
+        sizes , strings_pack = s.split("#")
+        sizes : list[int] = [ int(size) for size in sizes.split(",")]
+        
+        return_obj : list[str] = []
+        start : int = 0
+        #--------------------------------------------------
+        for str_len in sizes:
+            end : int = start + str_len
+            temp_str : str = strings_pack[start:end]
+            return_obj.append( temp_str )
+            start = end
+        #--------------------------------------------------
+
+        return return_obj
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+        
+
+
+print('------------------------------------')
+sol = Solution()
+
+input = ["neet","code","love","you"]
+expected = "4,4,4,3#neetcodeloveyou"
+result = sol.encode(strs=input)
+
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{expected == result}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{expected == result}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+#-----
+input = "4,4,4,3#neetcodeloveyou"
+expected = ["neet","code","love","you"]
+result = sol.decode(s = input)
+
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{expected == result}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{expected == result}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+
+
+# result : str = sol.decode(s = temp)
+
+
