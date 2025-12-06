57d9c8be860e87269456b8626caaf4b97a710562
Paulo <paulotcj@gmail.com>
2025-12-05 22:07:12 -0500

    .

---
+++ python/neetcode/7_Products of Array Except Self/1_Products of Array Except Self.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------
+class Solution:
+    #-------------------------------------------------------------------------
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        pass
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+
+
+print('------------------------------------')
+input = [1,2,4,6]
+expected = [48,24,12,8]
+
+sol = Solution()
+result = sol.productExceptSelf(nums=input)
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{is_equal}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{is_equal}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+
+print('------------------------------------')
+input = [-1,0,1,2,3]
+expected = [0,-6,0,0,0]
+
+sol = Solution()
+result = sol.productExceptSelf(nums=input)
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{is_equal}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{is_equal}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
