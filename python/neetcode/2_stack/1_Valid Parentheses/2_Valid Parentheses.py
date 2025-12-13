0187be958517e13a2e03f17a31da9bc8e1f81668
Paulo <paulotcj@gmail.com>
2025-12-12 20:12:57 -0500

    .

---
+++ python/neetcode/2_stack/1_Valid Parentheses/2_Valid Parentheses.py
@@ -0,0 +1,69 @@
+#-------------------------------------------------------------------------
+class Solution:
+    #-------------------------------------------------------------------------
+    def isValid(self, s: str) -> bool:
+        dict_closing_pairs : dict[str,str] = { ')':'(' ,  ']':'[' ,  '}':'{'    }
+        stack : list[str] = []
+
+        #--------------------------------------------------
+        for char in s :
+            if char in dict_closing_pairs : # closing char
+                if not stack : return False
+
+                char_pop = stack.pop()
+
+                if char_pop == dict_closing_pairs[char] : continue # pair matched
+                else: return False # pair didn't match
+            else: # opening char
+                stack.append(char)
+        #--------------------------------------------------
+
+        return len(stack) == 0
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+
+print('------------------------------------')
+input = "([{}])"
+expected = True
+
+sol = Solution()
+result = sol.isValid(s=input)
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{is_equal}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{is_equal}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+
+print('------------------------------------')
+input = "[]"
+expected = True
+
+sol = Solution()
+result = sol.isValid(s=input)
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{is_equal}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{is_equal}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+
+
+
+print('------------------------------------')
+input = "[(])"
+expected = False
+
+sol = Solution()
+result = sol.isValid(s=input)
+is_equal = expected == result
+if is_equal:
+    status_result = f"\033[1;37;42m{is_equal}\033[0m"  # Bold, white text, green background
+else:
+    status_result = f"\033[1;37;41m{is_equal}\033[0m"  # Bold, white text, red background
+print(f'is the result what was expected? {status_result} - expected: {expected} - result : {result}')
+
+
+        
